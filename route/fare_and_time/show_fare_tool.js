cd257c360ed6d03125492bb4202548d0073643fb
Eric Kuo <s88239@hotmail.com>
2014-05-16 17:25:10 +0800

    finished orange line. Add the function of showing fare table.

--- route/fare_and_time/show_fare_tool.js
+++ route/fare_and_time/show_fare_tool.js
@@ -1,8 +1,8 @@
-function create_select_menu(name_array, max_color_num, LINE_NAME){ // print the fair menu
+function create_select_menu(name_array, max_color_num, LINE_NAME){ // print the fare menu
 	var menu_name = ['from','to'];
-	document.write('<form name="fair_menu">');
+	document.write('<form name="fare_menu">');
 	for(var name_index = 0; name_index < menu_name.length ; ++name_index){
-		document.write('<select name="' + menu_name[name_index] + '" class="get_fair">');
+		document.write('<select name="' + menu_name[name_index] + '" class="get_fare">');
 		for(var i = 0, color_index = 1; i < name_array.length; ++i, ++color_index ){
 			for(var j = 0; j < name_array[i].length; ++j){
 				document.write('<option value="' + i + '" class="option' + color_index + '"');
@@ -14,23 +14,23 @@ function create_select_menu(name_array, max_color_num, LINE_NAME){ // print the 
 		}
 		document.write("</select>");
 	}
-	document.write('<input type="button" value="查詢" onClick="query_fair(this.form, ' + LINE_NAME + '_fair)">');
+	document.write('<input type="button" value="查詢" onClick="query_fare(this.form, ' + LINE_NAME + '_fare)">');
 	document.write('</form>');
 }
 
-function query_fair(theForm, fair_array){
+function query_fare(theForm, fare_array){
 	var fd = parseInt(theForm.from.value);
 	var td = parseInt(theForm.to.value);
-	var price = (fd<=td)?fair_array[td][fd]:fair_array[fd][td];
+	var price = (fd<=td)?fare_array[td][fd]:fare_array[fd][td];
 	//alert(fd + ' ' + td);
-	//alert(fair_array[td][fd]+" "+fair_array[fd][td]);
+	//alert(fare_array[td][fd]+" "+fare_array[fd][td]);
 	document.getElementById("cash_adult").innerHTML = price; // 現金全票
 	document.getElementById("cash_half").innerHTML = Math.ceil(price/2); // 現金半票，無條件進位
 	document.getElementById("RFID_adult").innerHTML = price-26; // 刷卡全票
 	document.getElementById("RFID_half").innerHTML = Math.ceil((price-26)/2); // 刷卡半票，無條件進位
 }
 
-function create_time_schedule(main_stop_name, time_consume, important_stop, time, isReturn){
+function create_time_schedule(main_stop_name, time_consume, important_stop, time, isReturn){ // print the time schedule
 	document.write('<table><tr>');
 	var start_idx = (isReturn==false) ? 0 : main_stop_name.length - 1;
 	var end_idx = (isReturn==false) ? main_stop_name.length : -1;
@@ -43,16 +43,16 @@ function create_time_schedule(main_stop_name, time_consume, important_stop, time
 		var start_stop = (isReturn==false) ? 0 : main_stop_name.length - 1;
 		var end_stop = (isReturn==false) ? main_stop_name.length - 1 : 0;
 		switch(time[i].length){
-			case 1:
+			case 1: // format: [time]
 				break;
-			case 2:
+			case 2: // format: [time, others]
 				special_tag = get_special_tag(time[i][1]);
 				break;
-			case 3:
+			case 3: // format: [time, start_stop, end_stop]
 				start_stop = time[i][1];
 				end_stop = time[i][2];
 				break;
-			case 4:
+			case 4: // format: [time, start_stop, end_stop , others]
 				start_stop = time[i][1];
 				end_stop = time[i][2];
 				special_tag = get_special_tag(time[i][3]);
@@ -77,19 +77,19 @@ function create_time_schedule(main_stop_name, time_consume, important_stop, time
 	document.write('</table>');
 }
 
-function get_special_tag(arr){
+function get_special_tag(arr){ // add some tags of special events
 	var special_event = ''; // initialize
 	for(var j=0; j<arr.length; ++j){
-		if(arr[j]=='W')
+		if(arr[j]=='W') // drive in weekdays
 			special_event += '<font color="#0000FF">W</font>';
-		else if(arr[j]=='L')
+		else if(arr[j]=='L') // use low floor bus
 			special_event += '<font color="#FF0000">L</font>';
 		else special_event += arr[j];
 	}
 	//alert(special_event);
 	return special_event;
 }
-function judge_important_stop(idx, arr){
+function judge_important_stop(idx, arr){ // find the index matching the important stop or not
 	for(var i=0; i<arr.length; ++i){
 		if(idx==arr[i]) return true;
 	}
@@ -108,4 +108,23 @@ function get_time(time_str, offset){ // get the time of the stop
 	min_zero = (time_min<10)?'0':'' ;
 	new_time = hr_zero + time_hr + ':' + min_zero + time_min;
 	return new_time;
+}
+function print_fare_table(interval_name, fare, color){ // the function fo showing or hiding the fare table
+	// print the button with the function showing or hiding
+	document.write('<input type="button" onClick="document.getElementById(\'fare_table\').style.display = ');
+	document.write('(document.getElementById(\'fare_table\').style.display==\'none\')?\'block\':\'none\'" ');
+	document.write('value="顯示/隱藏票價表格" style="background-color:#c00; color:#fff;">')
+	// print the fare table without showing
+	document.write('<div id="fare_table" style="display:none;"><table style="background-color: '+color+';">');
+	for(var i=0; i<interval_name.length; ++i){
+		document.write('<tr>');
+		for(var j=0; j<interval_name.length; ++j){
+			if(i==j) document.write('<th>' + interval_name[i] + '</th>');
+			else if( i>j ) document.write('<td>' + fare[i][j] + '</td>' );
+			else document.write('<td>' + (fare[j][i] - 26) + '</td>');
+		}
+		document.write('</tr>');
+	}
+	document.write('</table>');
+	document.write('<b>左方為現金價格，右方為刷卡價格</b><br /></div>');
 }
