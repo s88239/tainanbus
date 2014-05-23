5f78222eb2c1449299987b7fb5974c24e782b39f
Eric Kuo <s88239@hotmail.com>
2014-05-24 01:15:33 +0800

    finished the time schedule of city bus No.9

---
+++ route/fare_and_time/city_bus_9.js
@@ -0,0 +1,8 @@
+// format = [time at the start stop] or
+// [time, other] or
+// [time, start_stop, end_stop, other]
+city_bus_9_main_stop_name = ["臺南<br />火車站","臺南公園<br />(公園路)","延平<br />市場","太平橋","瀛海<br />中學","怡安<br />路口","總頭寮","新寮","公親里<br />活動中心","公親里"];
+city_bus_9_main_stop_time_consume = [0, 4, 8, 14, 17, 20, 23, 26, 28, 30];
+city_bus_9_important_stop = [0, 4, 9]; // 臺南火車站, 瀛海中學, 公親里
+city_bus_9_time_go = [["07:00"],["08:40"],["11:30"],["14:30"],["16:30"],["18:00"]];
+city_bus_9_time_return = [["06:20"],["08:00"],["09:30"],["12:00"],["15:00"],["17:10"],["18:50"]];
