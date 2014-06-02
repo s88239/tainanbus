1d41930c186877735d7a1d62b5b2b21169f6418d
poyuwu <asjh612@gmail.com>
2014-06-03 03:02:21 +0800

    unfinished train_time shown(already can get info)

--- tools/parse_train.php
+++ tools/parse_train.php
@@ -1,7 +1,16 @@
 <?php
 $url = $_POST['url'];
-$fp = file($url);
-echo '<xmp>';
+$searchdate = $_POST['searchdate'];
+$fromstation = $_POST['fromstation'];
+$tostation = $_POST['tostation'];
+$trainclass = $_POST['trainclass'];
+$fromtime = $_POST['fromtime'];
+$totime = $_POST['totime'];
+
+#$temp = $($url."&searchdate=".$searchdate."&fromstation=".$fromstation."&tostation=".$tostation."&trainclass=".$trainclass."&fromtime=".$fromtime."&totime=".$totime$fp = file($url));
+$fp = file($url."&searchdate=".$searchdate."&fromstation=".$fromstation."&tostation=".$tostation."&trainclass=".$trainclass."&fromtime=".$fromtime."&totime=".$totime);
+
+#echo '<xmp>';
 echo $fp[64].$fp[70].$fp[71];
 //print_r($fp);
 echo '</xmp>';
