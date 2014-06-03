43e1d4dac9a8b90b3b4f075843f00b0c5a9b6c63
poyuwu <asjh612@gmail.com>
2014-06-03 16:44:24 +0800

    test github php

--- tools/parse_train.php
+++ tools/parse_train.php
@@ -1,17 +1,16 @@
 <?php
 $url = $_POST['url'];
-$searchdate = $_POST['searchdate'];
-$fromstation = $_POST['fromstation'];
-$tostation = $_POST['tostation'];
-$trainclass = $_POST['trainclass'];
-$fromtime = $_POST['fromtime'];
-$totime = $_POST['totime'];
+#$searchdate = $_POST['searchdate'];
+#$fromstation = $_POST['fromstation'];
+#$tostation = $_POST['tostation'];
+#$trainclass = $_POST['trainclass'];
+#$fromtime = $_POST['fromtime'];
+#$totime = $_POST['totime'];
 
 #$temp = $($url."&searchdate=".$searchdate."&fromstation=".$fromstation."&tostation=".$tostation."&trainclass=".$trainclass."&fromtime=".$fromtime."&totime=".$totime$fp = file($url));
-$fp = file($url."&searchdate=".$searchdate."&fromstation=".$fromstation."&tostation=".$tostation."&trainclass=".$trainclass."&fromtime=".$fromtime."&totime=".$totime);
-
+$fp = file($url);#."&searchdate=".$searchdate."&fromstation=".$fromstation."&tostation=".$tostation."&trainclass=".$trainclass."&fromtime=".$fromtime."&totime=".$totime);
 #echo '<xmp>';
 echo $fp[64].$fp[70].$fp[71];
 //print_r($fp);
-echo '</xmp>';
+#echo '</xmp>';
 ?>
