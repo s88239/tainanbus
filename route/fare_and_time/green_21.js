b53039e7944735f66411083c37cc52cbc2ea99db
Eric Kuo <s88239@hotmail.com>
2014-06-04 02:14:25 +0800

    finished the time schedule of green 21~25

---
+++ route/fare_and_time/green_21.js
@@ -0,0 +1,22 @@
+green_21_interval_name = ["玉井","鹿陶洋","龜丹里","龜丹<br />油礦"];
+green_21_interval_stop = [
+["玉井","玉田里"], // 玉井
+["後旦","竹圍","鹿陶廟","鹿陶"], // 鹿陶洋
+["林口","蘆藤腳","東龜丹","龜丹里"], // 龜丹里
+["龜丹溫泉","牛坑","三層線","龜丹油礦"] // 龜丹油礦
+];
+green_21_stop_time_consume = [0,2,3,5,6,7,9,10,11,13,15,17,18,20];
+green_21_fare = [
+[26],
+[26,26],
+[26,26,26],
+[31,26,26,26]
+];
+// format = [time at the start stop] or
+// [time, other] or
+// [time, start_stop, end_stop, other]
+green_21_main_stop_name = ["玉井","鹿陶","龜丹里","龜丹<br />溫泉","龜丹<br />油礦"];
+green_21_main_stop_time_consume = [0, 7, 13, 15, 20];
+green_21_important_stop = [0, 3, 4]; // 玉井, 龜丹溫泉, 龜丹油礦
+green_21_time_go = [["8:45"],["11:45"],["14:50"]];
+green_21_time_return = [["9:10"],["12:10"],["15:15"]];
