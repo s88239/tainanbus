0144a1275973fa25d931ca11988676f945d546c1
Eric Kuo <s88239@hotmail.com>
2014-07-03 00:36:08 +0800

    separate yellow 12-1 from yellow 12

---
+++ route/fare_and_time/yellow_12_1.js
@@ -0,0 +1,20 @@
+yellow_12_1_interval_name = ["白河","河東","虎子墓"];
+yellow_12_1_interval_stop = [
+["白河站","念佛會","凹底"], // 白河
+["畚箕湖","河東","河東國小","潭墘"], // 河東
+["虎子墓(虎山里)","福顯宮"] // 虎子墓
+];
+yellow_12_1_stop_time_consume = [0,1,3,4,5,7,9,10,10];
+yellow_12_1_fare = [
+[26],
+[26,26],
+[26,26,26]
+];
+// format = [time at the start stop] or
+// [time, other] or
+// [time, start_stop, end_stop, other]
+yellow_12_1_main_stop_name = ["白河","河東國小","福顯宮"];
+yellow_12_1_main_stop_time_consume = [0, 5, 10];
+yellow_12_1_important_stop = [0, 2]; // 白河, 福顯宮
+yellow_12_1_time_go = [["6:20"],["10:40"],["17:30"]];
+yellow_12_1_time_return = [["6:30"],["10:50"],["17:40"]];
