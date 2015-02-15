0f4ab16e08191dc55f50603b27124ceada563833
Eric Kuo <s88239@hotmail.com>
2015-02-15 17:22:51 +0800

    Update 8042, 8046, 8046A

--- route/fare_and_time/highway_bus_8046A.js
+++ route/fare_and_time/highway_bus_8046A.js
@@ -14,7 +14,7 @@ highway_bus_8046A_fare = [
 // [time, other] or
 // [time, start_stop, end_stop, other]
 highway_bus_8046A_main_stop_name = ["臺南火車站","嘉南<br />藥理大學","路竹","岡山轉運站","楠梓站<br />(楠梓火車站)","高鐵左營站"];
-highway_bus_8046A_main_stop_time_consume = [0,20,35,50,70,80];
+highway_bus_8046A_main_stop_time_consume = [0,25,45,60,80,90];
 highway_bus_8046A_important_stop = [0, 3, 5]; // 臺南火車站, 岡山轉運站, 高鐵左營站
 highway_bus_8046A_time_go = [["07:30"],["11:30"],["14:00"],["17:45"]];
 highway_bus_8046A_time_return = [["08:00"],["10:00"],["12:20"],["15:50"]];
