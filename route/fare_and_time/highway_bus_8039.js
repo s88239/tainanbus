b7f0008e7faf6cdb7dacb9e2b82b9336fb12f58c
Eric Kuo <s88239@hotmail.com>
2015-04-25 15:38:11 +0800

    Add new line: 8039

---
+++ route/fare_and_time/highway_bus_8039.js
@@ -0,0 +1,16 @@
+highway_bus_8039_interval_name = ["保安轉運站","奇美<br />博物站","興達港"];
+highway_bus_8039_interval_stop = [
+["保安轉運站"],
+["奇美博物站"],
+["興達港"]
+];
+highway_bus_8039_fare = [
+];
+// format = [time at the start stop] or
+// [time, other] or
+// [time, start_stop, end_stop, other]
+highway_bus_8039_main_stop_name = ["保安轉運站","奇美博物館","嘉南大學","東方技術學院","興達遠洋漁港","興達港觀光漁市<br /><font color=\"red\">(假日延駛)</font>"];
+highway_bus_8039_main_stop_time_consume = [0,5,10,25,35,45];
+highway_bus_8039_important_stop = [0, 1, 4, 5]; // 保安轉運站, 奇美博物站, 興達遠洋漁港, 興達港觀光漁市
+highway_bus_8039_time_go = [["08:20"],["10:05"],["11:45"],["14:15"],["16:05"],["17:35"],["19:05"]];
+highway_bus_8039_time_return = [["07:35"],["09:20"],["11:00"],["13:30"],["15:20"],["16:50"],["18:20"]];
