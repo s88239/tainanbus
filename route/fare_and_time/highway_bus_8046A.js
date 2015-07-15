68c8fda797705fcdd02ecfffb46d68ec7c32fce4
Eric Kuo <s88239@hotmail.com>
2015-07-15 20:26:22 +0800

    Update the route map of 8046A

--- route/fare_and_time/highway_bus_8046A.js
+++ route/fare_and_time/highway_bus_8046A.js
@@ -13,7 +13,7 @@ highway_bus_8046A_fare = [
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-highway_bus_8046A_main_stop_name = ["臺南<br />火車站","西門路<br />二段","大林","臺南<br />航空站","奇美<br />博物館","嘉南<br />藥理大學","大湖<br />火車站","路竹","岡山<br />轉運站","楠梓<br />火車站","高鐵<br />左營站"];
+highway_bus_8046A_main_stop_name = ["臺南<br />火車站","西門路<br />二段","大林","臺南<br />航空站","奇美<br />博物館","嘉南<br />藥理大學","大湖<br />火車站","路竹","岡山<br />轉運站","捷運<br />青埔站","高鐵<br />左營站"];
 highway_bus_8046A_main_stop_time_consume = [0,6,15,22,25,28,40,45,60,80,90];
 highway_bus_8046A_important_stop = [0, 4, 6, 8, 10]; // 臺南火車站, 奇美博物館, 大湖火車站, 岡山轉運站, 高鐵左營站
 highway_bus_8046A_time_go = [["07:30"],["11:30"],["14:00"],["17:45"],["22:30",[[2,-5,6,-10]]]];
