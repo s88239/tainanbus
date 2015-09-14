fac5312efb581c159f7039cf189c0a73bd6c93c4
Eric Kuo <s88239@hotmail.com>
2015-09-14 22:44:38 +0800

    更新時刻表：黃3、棕3、棕10、棕11

--- route/fare_and_time/brown_11.js
+++ route/fare_and_time/brown_11.js
@@ -1,12 +1,11 @@
 brown_11_interval_name = ["學甲","宅子港","新芳里","紅茄萣","頂洲"];
-brown_11_interval_stop = [
+brown_11_interval_stop = [ // 2015.09.14 checked
 ["學甲(民權路)","學甲(中正路)","學甲","學甲國中"], // 學甲
 ["德安寮","宅子港"], // 宅子港
 ["新芳里"], // 新芳里
 ["紅茄萣"], // 紅茄萣
 ["頂洲"] // 頂洲
 ];
-brown_11_stop_time_consume = [0,2,3,5,7,9,15,17,20];
 brown_11_fare = [
 [26],
 [26,26],
@@ -20,5 +19,5 @@ brown_11_fare = [
 brown_11_main_stop_name = ["學甲","新芳里","頂洲"];
 brown_11_main_stop_time_consume = [0, 15, 20];
 brown_11_important_stop = [0, 2]; // 學甲, 頂洲
-brown_11_time_go = [["06:20"],["13:30"],["17:40"]];
-brown_11_time_return = [["06:40"],["13:50"],["18:00"]];
+brown_11_time_go = [["06:20"],["13:35"],["17:40"]];
+brown_11_time_return = [["06:40"],["13:55"],["18:00"]];
