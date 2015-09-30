588dee13801bc4ff15676a78e37ce8381563d065
Eric Kuo <s88239@hotmail.com>
2015-09-30 22:55:43 +0800

    更新時刻表：棕2；新增站點：紅幹線、紅3、藍21~24、棕2

--- route/fare_and_time/brown_2.js
+++ route/fare_and_time/brown_2.js
@@ -1,12 +1,11 @@
 brown_2_interval_name = ["新營","茄苳腳","鹽水","牛稠子","下中"];
-brown_2_interval_stop = [ // checked interval at Apr.16
-["新營站","圓環","三民路口","民治市政中心","新營高工","中正世家","電力公司","新營體育場","新北里","營新醫院","統聯客運"], // 新營
+brown_2_interval_stop = [ // 2015.09.30 checked
+["新營站","圓環(第一銀行)","三民路口","民治市政中心","新營高工","中正世家","電力公司","新營體育場","新北里","營新醫院","統聯客運"], // 新營
 ["茄苳腳","新營工業區","明達中學"], // 茄苳腳
-["竹圍尾","橋南里","鹽水","南榮科大","鹽水國小","武廟"], // 鹽水
+["竹圍尾","橋南里","鹽水","南榮科大","汫水國小","護安宮","汫水里","鹽水國小","武廟"], // 鹽水
 ["岸內","義稠里","牛稠子"], // 牛稠子
 ["二莊","樹仔腳","下中"] // 下中
 ];
-brown_2_stop_time_consume = [0,2,4,6,8,9,9,10,11,11,12,15,16,17,19,20,22,25,26,26,27,27,28,29,30];
 brown_2_fare = [
 [26],
 [26,26],
@@ -17,8 +16,9 @@ brown_2_fare = [
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-brown_2_main_stop_name = ["新營","統聯<br />客運","鹽水","下中"];
-brown_2_main_stop_time_consume = [0, 12, 22, 30];
-brown_2_important_stop = [0, 2, 3]; // 新營, 鹽水, 下中
-brown_2_time_go = [["07:50"],["09:50"],["13:50"],["15:50"]];
-brown_2_time_return = [["08:30"],["10:30"],["14:30"],["16:30"]];
+brown_2_main_stop_name = ["新營","統聯客運","鹽水","洪水國小","下中"];
+brown_2_main_stop_time_consume = [0, 12, 22, 29, 30];
+brown_2_important_stop = [0, 2, 4]; // 新營, 鹽水, 下中
+var skip_stop = 3;
+brown_2_time_go = [["07:50",[[skip_stop+1,14]]],["09:50",[[skip_stop+1,14]]],["13:50",[skip_stop]],["15:50",[skip_stop]]];
+brown_2_time_return = [["08:40",[[skip_stop,12,skip_stop-1,2]]],["10:40",[[skip_stop,12,skip_stop-1,2]]],["14:30",[skip_stop]],["16:30",[skip_stop]]];
