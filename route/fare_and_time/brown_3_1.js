001da7e82a789a5f4709c57e3a0b46e2594ca968
Eric Kuo <s88239@hotmail.com>
2016-01-07 22:38:51 +0800

    更新時刻表：黃幹線、黃12、棕3-1

--- route/fare_and_time/brown_3_1.js
+++ route/fare_and_time/brown_3_1.js
@@ -7,7 +7,6 @@ brown_3_1_interval_stop = [ // checked 20150512
 ["五興里","舊廍里"], // 五興里
 ["鐵線里"] // 鐵線里
 ];
-brown_3_1_stop_time_consume = [0,2,4,6,8,9,9,10,11,11,14,16,18,18,20,23,25];
 brown_3_1_fare = [
 [26],
 [26,26],
@@ -19,8 +18,8 @@ brown_3_1_fare = [
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-brown_3_1_main_stop_name = ["新營","紙廠","太宮<br />派出所","五興里","舊廍里","鐵線里"];
-brown_3_1_main_stop_time_consume = [0, 16, 18, 23, 27, 35];
-brown_3_1_important_stop = [0, 5]; // 新營, 鐵線里
-brown_3_1_time_go = [["07:45"],["10:50"],["15:05"]];
-brown_3_1_time_return = [["08:25"],["11:30"],["15:45"]];
+brown_3_1_main_stop_name = ["新營","台電公司","紙廠","太宮<br />派出所","五興里","舊廍里","鐵線里"];
+brown_3_1_main_stop_time_consume = [0, 15, 16, 19, 23, 27, 35];
+brown_3_1_important_stop = [0, 6]; // 新營, 鐵線里
+brown_3_1_time_go = [["07:45"],["10:50"],["14:50"]];
+brown_3_1_time_return = [["08:25"],["11:30"],["15:30"]];
