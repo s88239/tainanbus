753eb848f680ded2e5eaf35bcb4ee02ac436ab96
Eric Kuo <s88239@hotmail.com>
2016-01-17 17:53:14 +0800

    更新關於市民卡之票價訊息

--- tools/msg.js
+++ tools/msg.js
@@ -1,5 +1,5 @@
 MeteredMSG = '<h3><font color="red">刷電子票證(悠遊卡、一卡通)8公里免費</font></h3>';
-CitybusMSG = '<h3><font color="red">每月22日為「公車日」，當日持電子票證搭乘市區公車(府城客運)免費</font></h3><h3><font color="blue">使用台南市民卡，市區公車每段票只需付半價9元</font></h3>'
+CitybusMSG = '<h3><font color="red">每月22日為「公車日」，當日持電子票證搭乘市區公車(府城客運)免費</font></h3>'
 PromotionMSG = '<h2>使用電子票證時，上下車皆須刷卡</h2><br /><h3><font color="#7F3310">台鐵、公車2小時內轉乘公車減免9元(需使用電子票證，公車轉台鐵無優惠)</font></h3><h3>65歲以上長者刷台南市民卡敬老卡及身障人士刷台南市民卡愛心卡，可享大台南公車全線免費搭乘</h3>';
 TimeScheduleMSG = '<p><h2>除了起站為發車時間，其餘站點皆為預估時間，敬請提早候車。</h2></p>';
 W_message = '<font color="#0000FF">W</font>表示僅平日行駛';
