8e0b2729cd2df9e8fd29effe7561a99b9b6110da
Eric Kuo <s88239@hotmail.com>
2016-03-04 22:37:51 +0800

    更新路線：8046A(開進樹人醫專)、8035(開進實踐大學)

--- route/fare_and_time/highway_bus_8046A.js
+++ route/fare_and_time/highway_bus_8046A.js
@@ -7,14 +7,14 @@ highway_bus_8046A_interval_stop = [
 ["楠梓站"],
 ["高鐵左營站"]
 ];
-highway_bus_8046A_stop_time_consume = [0,20,30,45,60,70];
 highway_bus_8046A_fare = [
 ];
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-highway_bus_8046A_main_stop_name = ["臺南<br />火車站","西門路<br />二段","大林","臺南<br />航空站","奇美<br />博物館","嘉南<br />藥理大學","大湖<br />火車站","路竹","岡山<br />轉運站","捷運<br />青埔站","高鐵<br />左營站"];
-highway_bus_8046A_main_stop_time_consume = [0,6,15,22,25,28,40,45,60,80,90];
-highway_bus_8046A_important_stop = [0, 4, 6, 8, 10]; // 臺南火車站, 奇美博物館, 大湖火車站, 岡山轉運站, 高鐵左營站
-highway_bus_8046A_time_go = [["07:30"],["11:30"],["14:00"],["17:45"]];
-highway_bus_8046A_time_return = [["08:00"],["10:00"],["12:20"],["15:50"]];
+highway_bus_8046A_main_stop_name = ["臺南<br />火車站","西門路<br />二段","大林","臺南<br />航空站","奇美<br />博物館","嘉南<br />藥理大學","樹人<br />醫專","大湖<br />火車站","路竹","岡山<br />轉運站","捷運<br />青埔站","高鐵<br />左營站"];
+highway_bus_8046A_main_stop_time_consume = [0,6,15,22,25,28,35,40,45,60,80,90];
+highway_bus_8046A_important_stop = [0, 4, 7, 9, 11]; // 臺南火車站, 奇美博物館, 大湖火車站, 岡山轉運站, 高鐵左營站
+var skip_stop = 6; // 樹人醫專
+highway_bus_8046A_time_go = [["07:30"],["11:30"],["14:00"],["17:45",[skip_stop]]];
+highway_bus_8046A_time_return = [["08:00"],["10:00"],["12:20"],["15:50",[skip_stop]]];
