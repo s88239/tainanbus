18affc115f461f62391e5345681271bfcfd99e69
Eric Kuo <s88239@hotmail.com>
2016-05-12 23:13:57 +0800

    更新時刻表：黃12-1、7407

--- route/fare_and_time/yellow_12_1.js
+++ route/fare_and_time/yellow_12_1.js
@@ -16,5 +16,5 @@ yellow_12_1_fare = [
 yellow_12_1_main_stop_name = ["白河","河東國小","福顯宮"];
 yellow_12_1_main_stop_time_consume = [0, 5, 10];
 yellow_12_1_important_stop = [0, 2]; // 白河, 福顯宮
-yellow_12_1_time_go = [["06:20"],["10:20"],["17:40"]];
-yellow_12_1_time_return = [["06:30"],["10:30"],["17:50"]];
+yellow_12_1_time_go = [["06:10"],["10:20"],["17:40"]];
+yellow_12_1_time_return = [["06:20"],["10:30"],["17:50"]];
