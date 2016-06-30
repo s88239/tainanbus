45f07bff202b4679e1e0b5fc695eadf9821ca896
Eric Kuo <s88239@hotmail.com>
2016-06-30 22:59:45 +0800

    更新時刻表：高鐵快捷公車H31、H62

--- route/fare_and_time/hsr_shuttle_h31.js
+++ route/fare_and_time/hsr_shuttle_h31.js
@@ -1,9 +1,3 @@
-hsr_shuttle_h31_interval_name = ["安億橋","臺南市政府"];
-hsr_shuttle_h31_interval_stop = [
-["原住民文化會館"], // 安億橋延駛段
-["臺南市政府","臺南站","建興國中(孔廟)","延平郡王祠(國立臺南大學)","大林新城","生產路口","台糖嘉年華","臺南航空站","奇美博物館","高鐵臺南站"] // 主線
-];
-
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
@@ -12,15 +6,15 @@ hsr_shuttle_h31_main_stop_time_consume = [0, 10, 17, 25, 30, 40, 45, 60];
 hsr_shuttle_h31_important_stop = [0, 1, 5, 6, 7]; // 桂田酒店, 臺南市政府, 臺南航空站, 奇美博物館, 高鐵臺南站
 var start_stop = 1; // 臺南市政府
 var end_stop = 7; // 高鐵臺南站
-hsr_shuttle_h31_time_go = [["05:30",start_stop,end_stop],["06:00",start_stop,end_stop],["06:40",start_stop,end_stop],["07:00",start_stop,end_stop],["07:40",start_stop,end_stop],["08:00",start_stop,end_stop],["08:20",start_stop,end_stop],
-["08:40",start_stop,end_stop],["09:00",start_stop,end_stop],["09:20",start_stop,end_stop],["09:40",start_stop,end_stop],["10:00",start_stop,end_stop],["10:10"],["10:40",start_stop,end_stop],["11:00",start_stop,end_stop],["11:10"],
-["11:40",start_stop,end_stop],["12:00",start_stop,end_stop],["12:10"],["12:40",start_stop,end_stop],["13:00",start_stop,end_stop],["13:10"],["13:40",start_stop,end_stop],["14:00",start_stop,end_stop],["14:10"],["14:40",start_stop,end_stop],
-["15:00",start_stop,end_stop],["15:10"],["15:40",start_stop,end_stop],["16:00",start_stop,end_stop],["16:10"],["16:40",start_stop,end_stop],["17:00",start_stop,end_stop],["17:10"],["17:40",start_stop,end_stop],["18:00",start_stop,end_stop],
-["18:10"],["18:40",start_stop,end_stop],["19:00",start_stop,end_stop],["19:10"],["19:40",start_stop,end_stop],["20:00",start_stop,end_stop],["20:10"],["20:40",start_stop,end_stop],["21:00",start_stop,end_stop],["21:10"],
-["21:40",start_stop,end_stop],["22:00",start_stop,end_stop],["22:40",start_stop,end_stop],["22:55",start_stop,end_stop]];
-hsr_shuttle_h31_time_return = [["07:00",end_stop,start_stop],["07:40",end_stop,start_stop],["08:00",end_stop,start_stop],["08:20",end_stop,start_stop],["08:40",end_stop,start_stop],["09:00",end_stop,start_stop],["09:20"],
-["09:40",end_stop,start_stop],["10:00",end_stop,start_stop],["10:20"],["10:40",end_stop,start_stop],["11:00",end_stop,start_stop],["11:20"],["11:40",end_stop,start_stop],["12:00",end_stop,start_stop],["12:20"],["12:40",end_stop,start_stop],
-["13:00",end_stop,start_stop],["13:20"],["13:40",end_stop,start_stop],["14:00",end_stop,start_stop],["14:20"],["14:40",end_stop,start_stop],["15:00",end_stop,start_stop],["15:20"],["15:40",end_stop,start_stop],
-["16:00",end_stop,start_stop],["16:20"],["16:40",end_stop,start_stop],["17:00",end_stop,start_stop],["17:20"],["17:40",end_stop,start_stop],["18:00",end_stop,start_stop],["18:20"],["18:40",end_stop,start_stop],["19:00",end_stop,start_stop],
-["19:20"],["19:40",end_stop,start_stop],["20:00",end_stop,start_stop],["20:20"],["20:40",end_stop,start_stop],["21:00",end_stop,start_stop],["21:20",end_stop,start_stop],["21:40",end_stop,start_stop],["22:00",end_stop,start_stop],
-["22:20",end_stop,start_stop],["22:40",end_stop,start_stop],["23:20",end_stop,start_stop],["23:40",end_stop,start_stop],["00:00",end_stop,start_stop]];
+hsr_shuttle_h31_time_go = [["05:30",start_stop,end_stop],["06:00",start_stop,end_stop],["06:40",start_stop,end_stop],["07:00",start_stop,end_stop],["07:35",start_stop,end_stop],["07:50",start_stop,end_stop],["08:20",start_stop,end_stop],
+["08:35",start_stop,end_stop],["08:50",start_stop,end_stop],["09:20",start_stop,end_stop],["09:35",start_stop,end_stop],["09:40"],["10:20",start_stop,end_stop],["10:35",start_stop,end_stop],["10:40"],["11:20",start_stop,end_stop],
+["11:35",start_stop,end_stop],["11:40"],["12:20",start_stop,end_stop],["12:35",start_stop,end_stop],["12:40"],["13:20",start_stop,end_stop],["13:35",start_stop,end_stop],["13:40"],["14:20",start_stop,end_stop],["14:35",start_stop,end_stop],
+["14:40"],["15:20",start_stop,end_stop],["15:35",start_stop,end_stop],["15:40"],["16:20",start_stop,end_stop],["16:35",start_stop,end_stop],["16:40"],["17:20",start_stop,end_stop],["17:35",start_stop,end_stop],["17:40"],
+["18:20",start_stop,end_stop],["18:35",start_stop,end_stop],["18:40"],["19:20",start_stop,end_stop],["19:35",start_stop,end_stop],["19:40"],["20:20",start_stop,end_stop],["20:35",start_stop,end_stop],["20:40"],["21:20",start_stop,end_stop],
+["21:35",start_stop,end_stop],["21:50",start_stop,end_stop],["22:35",start_stop,end_stop],["22:55",start_stop,end_stop]];
+hsr_shuttle_h31_time_return = [["07:00",end_stop,start_stop],["07:35",end_stop,start_stop],["07:50",end_stop,start_stop],["08:20",end_stop,start_stop],["08:35",end_stop,start_stop],["08:50"],["09:20",end_stop,start_stop],
+["09:35",end_stop,start_stop],["09:50"],["10:20",end_stop,start_stop],["10:35",end_stop,start_stop],["10:50"],["11:20",end_stop,start_stop],["11:35",end_stop,start_stop],["11:50"],["12:20",end_stop,start_stop],["12:35",end_stop,start_stop],
+["12:50"],["13:20",end_stop,start_stop],["13:35",end_stop,start_stop],["13:50"],["14:20",end_stop,start_stop],["14:35",end_stop,start_stop],["14:50"],["15:20",end_stop,start_stop],["15:35",end_stop,start_stop],
+["15:50"],["16:20",end_stop,start_stop],["16:35",end_stop,start_stop],["16:50"],["17:20",end_stop,start_stop],["17:35",end_stop,start_stop],["17:50"],["18:20",end_stop,start_stop],["18:35",end_stop,start_stop],["18:50"],
+["19:20",end_stop,start_stop],["19:35",end_stop,start_stop],["19:50"],["20:20",end_stop,start_stop],["20:35",end_stop,start_stop],["20:50",end_stop,start_stop],["21:20",end_stop,start_stop],["21:35",end_stop,start_stop],["21:50",end_stop,start_stop],
+["22:20",end_stop,start_stop],["22:40",end_stop,start_stop],["23:20",end_stop,start_stop],["23:35",end_stop,start_stop],["00:00",end_stop,start_stop]];
--- route/fare_and_time/hsr_shuttle_h62.js
+++ route/fare_and_time/hsr_shuttle_h62.js
@@ -1,9 +1,3 @@
-hsr_shuttle_h62_interval_name = ["桂田酒店","奇美醫院"];
-hsr_shuttle_h62_interval_stop = [
-["桂田酒店","鹽行"], // 桂田酒店延駛段
-["奇美醫院","臺南高工","兵仔市","國賓影城","關帝廳","富強市場","崇明路口","文化中心","中華東、東門路口","富農街口","仁德交流道","仁德","高鐵臺南站"] // 主線
-];
-
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
@@ -12,13 +6,13 @@ hsr_shuttle_h62_main_stop_time_consume = [0, 15, 22, 25, 40, 50, 55, 80];
 hsr_shuttle_h62_important_stop = [0, 1, 3, 7]; // 桂田酒店, 奇美醫院, 南紡夢時代, 高鐵臺南站
 var start_stop = 1; // 奇美醫院
 var end_stop = 7; // 高鐵臺南站
-hsr_shuttle_h62_time_go = [["05:25",start_stop,end_stop],["06:00",start_stop,end_stop],["06:35",start_stop,end_stop],["07:00",start_stop,end_stop],["07:35",start_stop,end_stop],["08:00",start_stop,end_stop],["08:35",start_stop,end_stop],
-["08:45"],["09:20",start_stop,end_stop],["09:35",start_stop,end_stop],["10:00",start_stop,end_stop],["10:20"],["11:00",start_stop,end_stop],["11:20"],["12:00",start_stop,end_stop],["12:10",start_stop,end_stop],
-["12:35",start_stop,end_stop],["12:45"],["13:20"],["13:55",start_stop,end_stop],["14:15",start_stop,end_stop],["14:35",start_stop,end_stop],["15:00",start_stop,end_stop],["15:20"],["15:55",start_stop,end_stop],["16:05",start_stop,end_stop],
-["16:35",start_stop,end_stop],["17:00",start_stop,end_stop],["17:20"],["18:00",start_stop,end_stop],["18:10",start_stop,end_stop],["18:35",start_stop,end_stop],["19:00",start_stop,end_stop],["19:20"],["20:00",start_stop,end_stop],
-["20:30"],["21:00",start_stop,end_stop],["21:35",start_stop,end_stop],["22:00",start_stop,end_stop],["22:30",start_stop,end_stop]];
-hsr_shuttle_h62_time_return = [["07:00",end_stop,start_stop],["07:20"],["07:50",end_stop,start_stop],["08:20",end_stop,start_stop],["8:50"],["09:20",end_stop,start_stop],
-["09:50"],["10:20",end_stop,start_stop],["10:50",end_stop,start_stop],["11:10"],["11:20",end_stop,start_stop],["11:50"],["12:20",end_stop,start_stop],["12:50",end_stop,start_stop],["13:10",end_stop,start_stop],["13:20",end_stop,start_stop],
-["13:50"],["14:20",end_stop,start_stop],["14:50",end_stop,start_stop],["15:00",end_stop,start_stop],["15:20",end_stop,start_stop],["15:50"],["16:20",end_stop,start_stop],["16:50",end_stop,start_stop],["17:00",end_stop,start_stop],
-["17:20",end_stop,start_stop],["17:50"],["18:20",end_stop,start_stop],["18:50",end_stop,start_stop],["19:10"],["19:20",end_stop,start_stop],["19:50",end_stop,start_stop],["20:20",end_stop,start_stop],["20:50",end_stop,start_stop],
-["21:20",end_stop,start_stop],["21:50",end_stop,start_stop],["22:20",end_stop,start_stop],["22:50",end_stop,start_stop],["23:25",end_stop,start_stop],["23:55",end_stop,start_stop]];
+hsr_shuttle_h62_time_go = [["05:25",start_stop,end_stop],["05:50",start_stop,end_stop],["06:25",start_stop,end_stop],["07:00",start_stop,end_stop],["07:25",start_stop,end_stop],["08:00",start_stop,end_stop],["08:25",start_stop,end_stop],
+["08:45"],["09:25",start_stop,end_stop],["09:35",start_stop,end_stop],["10:00",start_stop,end_stop],["10:10"],["11:00",start_stop,end_stop],["11:10"],["12:00",start_stop,end_stop],["12:10",start_stop,end_stop],
+["12:35",start_stop,end_stop],["12:45"],["13:10"],["13:55",start_stop,end_stop],["14:15",start_stop,end_stop],["14:25",start_stop,end_stop],["15:00",start_stop,end_stop],["15:20"],["15:45",start_stop,end_stop],["16:05",start_stop,end_stop],
+["16:25",start_stop,end_stop],["17:00",start_stop,end_stop],["17:10"],["18:00",start_stop,end_stop],["18:10",start_stop,end_stop],["18:35",start_stop,end_stop],["19:00",start_stop,end_stop],["19:10"],["20:00",start_stop,end_stop],
+["20:25",start_stop,end_stop],["20:45"],["21:25",start_stop,end_stop],["22:00",start_stop,end_stop],["22:30",start_stop,end_stop]];
+hsr_shuttle_h62_time_return = [["07:00",end_stop,start_stop],["07:20"],["07:40",end_stop,start_stop],["08:20",end_stop,start_stop],["8:40"],["09:20",end_stop,start_stop],
+["09:40"],["10:20",end_stop,start_stop],["10:40",end_stop,start_stop],["11:10"],["11:20",end_stop,start_stop],["11:40"],["12:20",end_stop,start_stop],["12:40",end_stop,start_stop],["13:10",end_stop,start_stop],["13:20",end_stop,start_stop],
+["13:50"],["14:20",end_stop,start_stop],["14:40",end_stop,start_stop],["15:00",end_stop,start_stop],["15:20",end_stop,start_stop],["15:40"],["16:20",end_stop,start_stop],["16:50",end_stop,start_stop],["17:00",end_stop,start_stop],
+["17:20",end_stop,start_stop],["17:40"],["18:20",end_stop,start_stop],["18:40",end_stop,start_stop],["19:10",end_stop,start_stop],["19:20"],["19:50",end_stop,start_stop],["20:20",end_stop,start_stop],["20:40",end_stop,start_stop],
+["21:20",end_stop,start_stop],["21:50",end_stop,start_stop],["22:20",end_stop,start_stop],["22:40",end_stop,start_stop],["23:25",end_stop,start_stop],["23:55",end_stop,start_stop]];
