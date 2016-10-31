08b0b81952a1443d126788cd3d4b21fdee2bb514
Eric Kuo <s88239@hotmail.com>
2016-10-31 22:30:38 +0800

    修正7702台灣好行鹽鄉濱海線之路線資訊

--- route/fare_and_time/tour_bus_7702.js
+++ route/fare_and_time/tour_bus_7702.js
@@ -23,10 +23,5 @@ tour_bus_7702_fare = [
 tour_bus_7702_main_stop_name = ["嘉義<br />轉運站","嘉義<br />高鐵站","故宮南院","先天宮","東石<br />漁人碼頭","高跟鞋<br />教堂","好美里","南鯤鯓<br />代天府","北門<br />遊客中心"];
 tour_bus_7702_main_stop_time_consume = [0,20,30,50,52,70,85,95,100];
 tour_bus_7702_important_stop = [0, 1, 2, 5, 8]; // 嘉義轉運站, 嘉義高鐵站, 故宮南院, 高跟鞋教堂, 北門遊客中心
-var stop_1 = 2; // 故宮南院
-var stop_2 = 5; // 布袋遊客中心
-tour_bus_7702_time_go = [["08:10"],["12:50",[[stop_1,10]]]];
-tour_bus_7702_time_return = [["10:30",['觀',[stop_2,40]]],["14:40",[[stop_1-1,-5]]]];
-// holiday schedule
-tour_bus_7702_holiday_time_go = [["10:10"],["15:10"]];
-tour_bus_7702_holiday_time_return = [["12:30",['觀',[stop_2,40]]],["16:50"]];
+tour_bus_7702_time_go = [["09:00"],["11:30"]];
+tour_bus_7702_time_return = [["15:00"],["17:30"]];
