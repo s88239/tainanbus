8a6707db010246ad14f8db5c17e2af44887b5a81
Eric Kuo <s88239@hotmail.com>
2017-01-05 23:08:10 +0800

    更新時刻表：南科紅線、南科橘線、南科藍線

--- route/fare_and_time/STSP_orange.js
+++ route/fare_and_time/STSP_orange.js
@@ -4,6 +4,7 @@
 STSP_orange_main_stop_name = ["Park 17<br />商場","馨園<br />宿舍","南科<br />實中","民生<br />水塔","臺鐵<br />南科站","南科<br />實中","社區<br />中心","西拉雅<br />廣場","台積電","群創<br />B廠","聯電","Park 17<br />商場"];
 STSP_orange_main_stop_time_consume = [0, 3, 8, 3, 7, 10, 13, 16, 19, 21, 22, 24];
 STSP_orange_important_stop = [0, 4, 6, 8]; // Park 17商場, 臺鐵南科站, 社區中心, 台積電
+var start_stop = 0; // Park 17商場
 var stop_01 = 1; // 馨園宿舍
 var stop_02 = 2; // 南科實中
 var TRA_NK = 4; // 臺鐵南科站
@@ -12,7 +13,8 @@ var stop_2 = 6; // 社區中心
 var stop_3 = 7; // 西拉雅廣場
 STSP_orange_time_go = [["06:20",[stop_01,stop_02]],["06:41",[stop_01,stop_02]],["07:05",[stop_01,stop_02,[stop_1,1]]],["07:16",[[stop_02+1,8,stop_1,1,stop_2,2,stop_3,1]]],
 ["07:49",[stop_01,stop_02,[stop_1,1,stop_2,2,stop_3,1]]],["07:57",[stop_01,stop_02,[stop_1,1,stop_2,2,stop_3,1]]],["08:25",[stop_01,stop_02,[stop_1,1]]],["09:06",[stop_01,stop_02]],["09:35",[stop_01,stop_02]],
-["10:33",[stop_01,stop_02]],["11:05",[stop_01,stop_02]],["11:50",[stop_01,stop_02,'*',[TRA_NK,21]]],["12:49",[stop_01,stop_02]],["13:31",[stop_01,stop_02]],["13:50",[stop_01,stop_02]],["14:39",[stop_01,stop_02]],["15:30",[stop_01,stop_02]],["16:06",[stop_01,[stop_02,-3,stop_02+1,8]]],["17:03",[stop_01,stop_02]]
+["10:33",[stop_01,stop_02]],["11:05",[stop_01,stop_02]],["11:50",[stop_01,stop_02,'*',[TRA_NK,21]]],["12:49",[stop_01,stop_02]],["13:31",[stop_01,stop_02]],["13:50",[stop_01,stop_02]],["14:39",[stop_01,stop_02]],
+["15:30",[stop_01,stop_02]],["16:06",[stop_01,[stop_02,-3,stop_02+1,8]]],["17:03",[stop_01,stop_02]],["17:00",start_stop,TRA_NK,[stop_01,[stop_02,-3,stop_02+1,8,TRA_NK,1]]],["17:18",stop_02,TRA_NK,[[stop_02+1,2,TRA_NK,6]]]
 ];
 
 var return_stop_1 = 6; // 南科實中校門口
@@ -20,4 +22,4 @@ var return_stop_1 = 6; // 南科實中校門口
 STSP_orange_main_stop_name_return = ["Park 17<br />商場","聯電","霞客湖<br />停車場","南茂<br />科技","商務<br />會館","華園<br />宿舍","南科實中<br />校門口","民生<br />水塔","臺鐵<br />南科站","南科<br />實中","社區<br />中心","Park 17<br />商場"];
 STSP_orange_main_stop_time_consume_return = [0, 2, 4, 6, 9, 11, 15, 13, 17, 20, 22, 25];
 STSP_orange_important_stop_return = [0, 3, 5, 8]; // Park 17商場, 南茂科技, 華園宿舍, 臺鐵南科站
-STSP_orange_time_return = [["17:07",[return_stop_1]],["17:39",[return_stop_1]],["18:05",[return_stop_1]],["18:22",[return_stop_1]],["18:57",[return_stop_1]],["19:18",[return_stop_1]],["20:12",[return_stop_1]],["20:38",[[return_stop_1+1,5]]],["21:24",[return_stop_1]]];
+STSP_orange_time_return = [["17:07",[return_stop_1]],["17:39",[return_stop_1]],["18:05",[return_stop_1]],["18:22",[return_stop_1]],["18:57",[return_stop_1]],["19:20",[return_stop_1]],["20:12",[return_stop_1]],["20:38",[[return_stop_1+1,5]]],["21:24",[return_stop_1]]];
--- route/fare_and_time/STSP_red.js
+++ route/fare_and_time/STSP_red.js
@@ -11,16 +11,16 @@ var stop_3 = 3; // 民生水塔
 var TRA_NK = 4;
 var stop_4 = 11; // 南科健康生活館
 STSP_red_time_go = [["06:19",[stop_1,stop_2,[stop_4+3,1]]],["06:30",stop_1,TRA_NK+1,['*',stop_2,stop_3,[TRA_NK,10,TRA_NK+1,1]]],
-["06:40",[stop_1,stop_2]],["07:05",stop_1,stop_2],["07:03",[stop_1,stop_2,[TRA_NK+1,1]]],
+["06:40",[stop_1,stop_2]],["06:56",stop_1,TRA_NK+1,[stop_3,[TRA_NK,6]]],["07:03",[stop_1,stop_2,[TRA_NK+1,1]]],
 ["07:16",stop_1,TRA_NK+1,[stop_3,[TRA_NK,5,TRA_NK+1,1]]],["07:23",[stop_1,stop_2,[TRA_NK+1,1,TRA_NK+2,2,TRA_NK+3,1]]],
 ["07:49",[stop_1,stop_2,[TRA_NK+1,1,TRA_NK+2,2,TRA_NK+3,1]]],["07:49",[stop_1,stop_2,[TRA_NK+1,1,TRA_NK+2,2,TRA_NK+3,1]]],
 ["07:58",[stop_1,stop_2,[TRA_NK+1,1,TRA_NK+2,2,TRA_NK+3,1]]],["08:25",[stop_1,stop_2,[TRA_NK+1,1]]],["08:26",[stop_1,stop_2,[TRA_NK+1,1]]],["09:05",[stop_1,stop_2]],
 ["09:25",[stop_1,stop_2]],["10:00",[stop_1,stop_2]],["10:35",[stop_1,stop_2]],["11:33",[stop_1,stop_2]],["12:30",[stop_1,stop_2]],
-["13:30",[stop_1,stop_2]],["14:00",[stop_1,stop_2]],["14:40",[stop_1,stop_2]],["15:29",[stop_1,stop_2]],["16:25",[stop_1,stop_2]]
+["13:30",[stop_1,stop_2]],["14:00",[stop_1,stop_2]],["14:40",[stop_1,stop_2]],["15:30",[stop_1,stop_2]],["16:25",[stop_1,stop_2]]
 ];
 
 // The name of return stops are different
 STSP_red_main_stop_name_return = ["Park 17<br />商場","群創<br />C廠","台達電","台灣<br />康寧","南科健康<br />生活館","廣停3","漢民<br />科技","東捷<br />科技","茂迪","華園<br />宿舍","民生<br />水塔","臺鐵<br />南科站","南科<br />實中","社區<br />中心","Park 17<br />商場"];
 STSP_red_main_stop_time_consume_return = [0, 3, 6, 7, 8, 9, 10, 11, 13, 16, 18, 22, 25, 27, 30];
 STSP_red_important_stop_return = [0, 2, 9, 11]; // Park 17商場, 台達電, 華園宿舍, 臺鐵南科站
-STSP_red_time_return = [["17:03"],["17:33"],["17:38"],["17:53"],["18:17"],["18:52"],["19:13"],["20:06"],["20:38"],["21:19"]];
+STSP_red_time_return = [["17:03"],["17:33"],["17:37",[[1,1]]],["17:53"],["18:17"],["18:52"],["19:13"],["20:06"],["20:38"],["21:19"]];
