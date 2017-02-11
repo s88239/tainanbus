208fb7d6b3dd560e9dca6ed939c5e3b7d2a21886
Eric Kuo <s88239@hotmail.com>
2017-02-11 22:45:45 +0800

    新增站位：H62(增「義林一街口」)、H31(增「國平路口」(往高鐵))； 站位更名：H31(「興南客運台南站」更名為「永華站」(往高鐵)與「小西門(大億麗緻)」(往市府))

--- route/fare_and_time/hsr_shuttle_h31.js
+++ route/fare_and_time/hsr_shuttle_h31.js
@@ -1,9 +1,9 @@
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-hsr_shuttle_h31_main_stop_name = ["原住民文化會館<br /><font color=\"red\">(假日延駛)</font>","臺南市政府","興南客運<br />臺南站","延平郡王祠<br />(國立臺南大學)","大林新城","臺南航空站","奇美博物館","高鐵臺南站"];
+hsr_shuttle_h31_main_stop_name = ["原住民文化會館<br /><span style='color: red;'>(假日延駛)</span>","臺南市政府","永華站","延平郡王祠<br />(國立臺南大學)","大林新城","臺南航空站<br />(奇美幸福工廠)","奇美博物館","高鐵臺南站"];
 hsr_shuttle_h31_main_stop_time_consume = [0, 10, 17, 25, 30, 40, 45, 60];
-hsr_shuttle_h31_important_stop = [0, 1, 5, 6, 7]; // 桂田酒店, 臺南市政府, 臺南航空站, 奇美博物館, 高鐵臺南站
+hsr_shuttle_h31_important_stop = [0, 1, 5, 6, 7]; // 原住民文化會館, 臺南市政府, 臺南航空站(奇美幸福工廠), 奇美博物館, 高鐵臺南站
 var start_stop = 1; // 臺南市政府
 var end_stop = 7; // 高鐵臺南站
 hsr_shuttle_h31_time_go = [["05:30",start_stop,end_stop],["06:00",start_stop,end_stop],["06:40",start_stop,end_stop],["07:00",start_stop,end_stop],["07:35",start_stop,end_stop],["07:50",start_stop,end_stop],["08:20",start_stop,end_stop],
@@ -12,9 +12,16 @@ hsr_shuttle_h31_time_go = [["05:30",start_stop,end_stop],["06:00",start_stop,end
 ["14:40"],["15:20",start_stop,end_stop],["15:35",start_stop,end_stop],["15:40"],["16:20",start_stop,end_stop],["16:35",start_stop,end_stop],["16:40"],["17:20",start_stop,end_stop],["17:35",start_stop,end_stop],["17:40"],
 ["18:20",start_stop,end_stop],["18:35",start_stop,end_stop],["18:40"],["19:20",start_stop,end_stop],["19:35",start_stop,end_stop],["19:40"],["20:20",start_stop,end_stop],["20:35",start_stop,end_stop],["20:40"],["21:20",start_stop,end_stop],
 ["21:35",start_stop,end_stop],["21:50",start_stop,end_stop],["22:35",start_stop,end_stop],["22:55",start_stop,end_stop]];
-hsr_shuttle_h31_time_return = [["07:00",end_stop,start_stop],["07:35",end_stop,start_stop],["07:50",end_stop,start_stop],["08:20",end_stop,start_stop],["08:35",end_stop,start_stop],["08:50"],["09:20",end_stop,start_stop],
-["09:35",end_stop,start_stop],["09:50"],["10:20",end_stop,start_stop],["10:35",end_stop,start_stop],["10:50"],["11:20",end_stop,start_stop],["11:35",end_stop,start_stop],["11:50"],["12:20",end_stop,start_stop],["12:35",end_stop,start_stop],
-["12:50"],["13:20",end_stop,start_stop],["13:35",end_stop,start_stop],["13:50"],["14:20",end_stop,start_stop],["14:35",end_stop,start_stop],["14:50"],["15:20",end_stop,start_stop],["15:35",end_stop,start_stop],
-["15:50"],["16:20",end_stop,start_stop],["16:35",end_stop,start_stop],["16:50"],["17:20",end_stop,start_stop],["17:35",end_stop,start_stop],["17:50"],["18:20",end_stop,start_stop],["18:35",end_stop,start_stop],["18:50"],
-["19:20",end_stop,start_stop],["19:35",end_stop,start_stop],["19:50"],["20:20",end_stop,start_stop],["20:35",end_stop,start_stop],["20:50",end_stop,start_stop],["21:20",end_stop,start_stop],["21:35",end_stop,start_stop],["21:50",end_stop,start_stop],
-["22:20",end_stop,start_stop],["22:40",end_stop,start_stop],["23:20",end_stop,start_stop],["23:35",end_stop,start_stop],["00:00",end_stop,start_stop]];
+
+// The name of return stops are different
+hsr_shuttle_h31_main_stop_name_return = ["高鐵臺南站","奇美博物館","臺南航空站<br />(奇美幸福工廠)","大林新城","延平郡王祠<br />(國立臺南大學)","小西門<br />(大億麗緻)","臺南市政府","原住民文化會館<br /><span style='color: red;'>(假日延駛)</span>"];
+hsr_shuttle_h31_main_stop_time_consume_return = [0, 15, 20, 30, 35, 43, 50, 60];
+hsr_shuttle_h31_important_stop_return = [0, 1, 2, 6, 7]; // 高鐵臺南站, 奇美博物館, 臺南航空站(奇美幸福工廠), 臺南市政府, 原住民文化會館
+var start_stop_return = 0; // 高鐵臺南站
+var end_stop_return = 6; // 臺南市政府
+hsr_shuttle_h31_time_return = [["07:00",start_stop_return,end_stop_return],["07:35",start_stop_return,end_stop_return],["07:50",start_stop_return,end_stop_return],["08:20",start_stop_return,end_stop_return],["08:35",start_stop_return,end_stop_return],["08:50"],["09:20",start_stop_return,end_stop_return],
+["09:35",start_stop_return,end_stop_return],["09:50"],["10:20",start_stop_return,end_stop_return],["10:35",start_stop_return,end_stop_return],["10:50"],["11:20",start_stop_return,end_stop_return],["11:35",start_stop_return,end_stop_return],["11:50"],["12:20",start_stop_return,end_stop_return],["12:35",start_stop_return,end_stop_return],
+["12:50"],["13:20",start_stop_return,end_stop_return],["13:35",start_stop_return,end_stop_return],["13:50"],["14:20",start_stop_return,end_stop_return],["14:35",start_stop_return,end_stop_return],["14:50"],["15:20",start_stop_return,end_stop_return],["15:35",start_stop_return,end_stop_return],
+["15:50"],["16:20",start_stop_return,end_stop_return],["16:35",start_stop_return,end_stop_return],["16:50"],["17:20",start_stop_return,end_stop_return],["17:35",start_stop_return,end_stop_return],["17:50"],["18:20",start_stop_return,end_stop_return],["18:35",start_stop_return,end_stop_return],["18:50"],
+["19:20",start_stop_return,end_stop_return],["19:35",start_stop_return,end_stop_return],["19:50"],["20:20",start_stop_return,end_stop_return],["20:35",start_stop_return,end_stop_return],["20:50",start_stop_return,end_stop_return],["21:20",start_stop_return,end_stop_return],["21:35",start_stop_return,end_stop_return],["21:50",start_stop_return,end_stop_return],
+["22:20",start_stop_return,end_stop_return],["22:40",start_stop_return,end_stop_return],["23:20",start_stop_return,end_stop_return],["23:35",start_stop_return,end_stop_return],["00:00",start_stop_return,end_stop_return]];
--- route/fare_and_time/hsr_shuttle_h62.js
+++ route/fare_and_time/hsr_shuttle_h62.js
@@ -1,11 +1,11 @@
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-hsr_shuttle_h62_main_stop_name = ["桂田酒店","奇美醫院","兵仔市","南紡夢時代","文化中心","富農街口","仁德交流道","高鐵臺南站"];
-hsr_shuttle_h62_main_stop_time_consume = [0, 15, 22, 25, 40, 50, 55, 80];
-hsr_shuttle_h62_important_stop = [0, 1, 3, 7]; // 桂田酒店, 奇美醫院, 南紡夢時代, 高鐵臺南站
+hsr_shuttle_h62_main_stop_name = ["桂田酒店","奇美醫院","兵仔市","南紡<br />購物中心","文化中心","富農街口","仁德交流道","義林一街口","高鐵臺南站"];
+hsr_shuttle_h62_main_stop_time_consume = [0, 15, 22, 25, 35, 42, 45, 50, 65];
+hsr_shuttle_h62_important_stop = [0, 1, 3, 6, 8]; // 桂田酒店, 奇美醫院, 南紡購物中心, 仁德交流道, 高鐵臺南站
 var start_stop = 1; // 奇美醫院
-var end_stop = 7; // 高鐵臺南站
+var end_stop = 8; // 高鐵臺南站
 hsr_shuttle_h62_time_go = [["05:25",start_stop,end_stop],["05:50",start_stop,end_stop],["06:25",start_stop,end_stop],["07:00",start_stop,end_stop],["07:25",start_stop,end_stop],["08:00",start_stop,end_stop],["08:25",start_stop,end_stop],
 ["08:45"],["09:25",start_stop,end_stop],["09:35",start_stop,end_stop],["10:00",start_stop,end_stop],["10:10"],["11:00",start_stop,end_stop],["11:10"],["12:00",start_stop,end_stop],["12:10",start_stop,end_stop],
 ["12:35",start_stop,end_stop],["12:45"],["13:10"],["13:55",start_stop,end_stop],["14:15",start_stop,end_stop],["14:25",start_stop,end_stop],["15:00",start_stop,end_stop],["15:20"],["15:45",start_stop,end_stop],["16:05",start_stop,end_stop],
