831bfd389400cc72a4e90df033aae74db2771fc2
Eric Kuo <s88239@hotmail.com>
2017-02-17 23:01:21 +0800

    更新時刻表：黃9(新營06:40發車班次改為06:50發)

--- route/fare_and_time/yellow_9.js
+++ route/fare_and_time/yellow_9.js
@@ -29,7 +29,7 @@ var skip_stop = 1; // 臺南市政府民治中心
 var stop_1 = 2; // 東興國宅
 var stop_2 = 6; // 後壁火車站
 var hsr_chiayi = 8; // 上茄苳
-yellow_9_time_go = [["06:00",start_stop,hsr_chiayi,[skip_stop,'W']],["06:40",start_stop,hsr_chiayi,[[stop_1,6,stop_2,4]]],["07:00",start_stop,hsr_chiayi],["08:00"],["09:00"],["10:00"],["11:00"],["12:00"],["13:00"],["14:00"],["15:00"],
+yellow_9_time_go = [["06:00",start_stop,hsr_chiayi,[skip_stop,'W']],["06:50",start_stop,hsr_chiayi],["07:00",start_stop,hsr_chiayi],["08:00"],["09:00"],["10:00"],["11:00"],["12:00"],["13:00"],["14:00"],["15:00"],
 ["16:00"],["17:00",[[stop_1,6,stop_2,4]]],["17:40",start_stop,hsr_chiayi,['五']],["18:00",start_stop,hsr_chiayi],["18:40",start_stop,hsr_chiayi,[skip_stop,'五']],["19:00",start_stop,hsr_chiayi,[skip_stop]],["19:40",start_stop,hsr_chiayi,[skip_stop,'五']],["20:00",start_stop,hsr_chiayi,[skip_stop]],["20:40",start_stop,hsr_chiayi,[skip_stop,'五']]];
 yellow_9_time_return = [["07:15",hsr_chiayi,start_stop,['W']],["07:55",hsr_chiayi,start_stop],["08:25",hsr_chiayi,start_stop],["09:10",[[hsr_chiayi,8]]],["10:10",[[hsr_chiayi,8]]],["11:10",[[hsr_chiayi,8]]],["12:10",[[hsr_chiayi,8]]],["13:10",[[hsr_chiayi,8]]],["14:10",[[hsr_chiayi,8]]],["15:10",[[hsr_chiayi,8]]],["16:10",[[hsr_chiayi,8]]],
 ["17:10",[[hsr_chiayi,8]]],["18:10",[skip_stop,[hsr_chiayi,8]]],["19:00",hsr_chiayi,start_stop,[skip_stop,'五']],["19:25",hsr_chiayi,start_stop,[skip_stop]],["20:00",hsr_chiayi,start_stop,[skip_stop,'五']],["20:25",hsr_chiayi,start_stop,[skip_stop]],["21:00",hsr_chiayi,start_stop,[skip_stop,'五']],["21:25",hsr_chiayi,start_stop,[skip_stop]],["22:00",hsr_chiayi,start_stop,[skip_stop,'五']]];
