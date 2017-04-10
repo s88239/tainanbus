af68681a847541a78db120c0240b667d6dee722b
Eric Kuo <s88239@hotmail.com>
2017-04-10 22:31:30 +0800

    更新時刻表：南科綠線(高鐵線)、7408(新營－好美里)

--- route/fare_and_time/STSP_green.js
+++ route/fare_and_time/STSP_green.js
@@ -4,10 +4,10 @@
 STSP_green_main_stop_name = ["Park 17商場","育成中心","高鐵臺南站"];
 STSP_green_main_stop_time_consume = [0, 2, 35];
 STSP_green_important_stop = [0, 2]; // Park 17商場, 高鐵臺南站
-STSP_green_time_go = [["05:55",[[2,-2]]],["08:00"],["10:00"],["14:00"],["16:00"],["18:00"]];
+STSP_green_time_go = [["05:55",[[2,-2]]],["06:40"],["07:25"],["08:25"],["09:25"],["10:00"],["11:25"],["13:25"],["14:55"],["15:55"],["16:55"],["18:00"]];
 
 // The name of return stops are different
 STSP_green_main_stop_name_return = ["高鐵臺南站","動物中心","Park 17商場"];
 STSP_green_main_stop_time_consume_return = [0, 33, 35];
 STSP_green_important_stop_return = [0, 2]; // 高鐵臺南站, Park 17商場
-STSP_green_time_return = [["06:50"],["08:50"],["10:50"],["14:50"],["16:50"],["18:50"]];
+STSP_green_time_return = [["06:50"],["07:30"],["08:15"],["09:25"],["10:25"],["10:45"],["12:25"],["14:20"],["15:45"],["16:45"],["17:45"],["18:50"]];
