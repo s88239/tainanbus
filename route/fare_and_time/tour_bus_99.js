1f505539f533bceecc9314ea4144a87d630a4219
Eric Kuo <s88239@hotmail.com>
2017-05-08 22:51:39 +0800

    更新時刻表：台灣好行99路

--- route/fare_and_time/tour_bus_99.js
+++ route/fare_and_time/tour_bus_99.js
@@ -1,19 +1,22 @@
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-tour_bus_99_main_stop_name = ["臺南公園<br />(公園路)","臺南<br />火車站","赤崁樓","接官亭","安平古堡<br />(安北路)","四草生態文化<br />園區(大眾廟)","四草野生<br />動物保護區","鹿耳門<br />聖母廟","臺灣鹽<br />博物館","七股<br />鹽山"];
-tour_bus_99_main_stop_time_consume = [0, 15, 25, 30, 40, 55, 60, 70, 100, 110];
-tour_bus_99_important_stop = [0, 1, 6, 9, 10]; // 臺南公園(公園路), 臺南火車站, 四草野生動物保護區, 七股鹽山, 黑面琵鷺賞鳥亭
+tour_bus_99_main_stop_name = ["臺南公園<br />(公園路)","臺南<br />火車站","赤崁樓","接官亭","安平古堡<br />(安北路)","四草生態文化<br />園區(大眾廟)","四草野生<br />動物保護區","鹿耳門<br />聖母廟","七股<br />鹽山"];
+tour_bus_99_main_stop_time_consume = [0, 15, 20, 27, 37, 48, 50, 60, 90];
+tour_bus_99_important_stop = [0, 1, 4, 6, 8]; // 臺南公園(公園路), 臺南火車站, 安平古堡(安北路), 四草野生動物保護區, 七股鹽山
+var start_stop = 0; // 臺南公園(公園路)
+var tra_tainan = 1; // 臺南火車站
 var stop_1 = 6; // 四草野生動物保護區
-tour_bus_99_time_go = [["09:45"],["10:45"],["14:45"]];
-tour_bus_99_time_return = [["11:55"],["12:55"],["16:55",[[stop_1,15]]]];
+var stop_2 = 7; // 鹿耳門聖母廟
+tour_bus_99_time_go = [["08:45",[[tra_tainan,-12,tra_tainan+1,12]]],["10:45",[[tra_tainan,-12,tra_tainan+1,12]]],["13:45",[[tra_tainan,-12,tra_tainan+1,12]]]];
+tour_bus_99_time_return = [["10:55",[[stop_2,-5]]],["12:55",[[stop_2,-5]]],["15:55",[[stop_2,-5]]]];
 // holiday schedule
-tour_bus_99_holiday_time_go = [["08:45"],["09:15",0,6],["09:45"],["10:15",0,6],["10:45"],["11:15",0,6],["11:45"],["12:15",0,6],
-["12:45"],["13:15",0,6],["13:45"],["14:15",0,6],["14:45"],["15:15",0,6],["15:45"],["16:15",0,6],["16:45",0,6],["17:15",0,6]
+tour_bus_99_holiday_time_go = [["08:45",[[tra_tainan,-12,tra_tainan+1,12]]],["09:15",start_stop,stop_1,[[tra_tainan,-12,tra_tainan+1,12]]],["09:45",[[tra_tainan,-12,tra_tainan+1,12]]],["10:15",start_stop,stop_1,[[tra_tainan,-12,tra_tainan+1,12]]],["10:45",[[tra_tainan,-12,tra_tainan+1,12]]],["11:15",start_stop,stop_1,[[tra_tainan,-12,tra_tainan+1,12]]],["11:45",[[tra_tainan,-12,tra_tainan+1,12]]],["12:15",start_stop,stop_1,[[tra_tainan,-12,tra_tainan+1,12]]],
+["12:45",[[tra_tainan,-12,tra_tainan+1,12]]],["13:15",start_stop,stop_1,[[tra_tainan,-12,tra_tainan+1,12]]],["13:45",[[tra_tainan,-12,tra_tainan+1,12]]],["14:15",start_stop,stop_1,[[tra_tainan,-12,tra_tainan+1,12]]],["14:45",[[tra_tainan,-12,tra_tainan+1,12]]],["15:15",start_stop,stop_1,[[tra_tainan,-12,tra_tainan+1,12]]],["15:45",[[tra_tainan,-12,tra_tainan+1,12]]],["16:15",start_stop,stop_1,[[tra_tainan,-12,tra_tainan+1,12]]],["16:45",start_stop,stop_1,[[tra_tainan,-12,tra_tainan+1,12]]],["17:15",start_stop,stop_1,[[tra_tainan,-12,tra_tainan+1,12]]]
 ];
-tour_bus_99_holiday_time_return = [["10:15",6,0],["11:15",6,0],["10:55"],["12:15",6,0],["11:55"],["13:15",6,0],["12:55"],
-["14:15",6,0],["13:55"],["15:15",6,0],["14:55"],["16:15",6,0],["15:55"],["17:15",6,0],["17:45",6,0],["16:55",[[stop_1,15]]],["18:15",6,0],
-["18:10"]];
+tour_bus_99_holiday_time_return = [["10:15",stop_1,start_stop],["11:15",stop_1,start_stop],["10:55",[[stop_2,-5]]],["12:15",stop_1,start_stop],["11:55",[[stop_2,-5]]],["13:15",stop_1,start_stop],["12:55",[[stop_2,-5]]],
+["14:15",stop_1,start_stop],["13:55",[[stop_2,-5]]],["15:15",stop_1,start_stop],["14:55",[[stop_2,-5]]],["16:15",stop_1,start_stop],["15:55",[[stop_2,-5]]],["17:15",stop_1,start_stop],["16:55",[[stop_2,-5]]],["18:00",stop_1,start_stop],["18:15",stop_1,start_stop],
+["18:10",[[stop_2,-5]]]];
 // vacation schedule
-tour_bus_99_vacation_time_go = [["08:45"],["09:45"],["10:45"],["11:45"],["12:45"],["13:45"],["14:45"],["15:45"]];
-tour_bus_99_vacation_time_return = [["10:55"],["11:55"],["12:55"],["13:55"],["14:55"],["15:55"],["16:55",[[stop_1,15]]],["18:10"]];
+tour_bus_99_vacation_time_go = [["08:45",[[tra_tainan,-12,tra_tainan+1,12]]],["09:45",[[tra_tainan,-12,tra_tainan+1,12]]],["10:45",[[tra_tainan,-12,tra_tainan+1,12]]],["11:45",[[tra_tainan,-12,tra_tainan+1,12]]],["12:45",[[tra_tainan,-12,tra_tainan+1,12]]],["13:45",[[tra_tainan,-12,tra_tainan+1,12]]],["14:45",[[tra_tainan,-12,tra_tainan+1,12]]],["15:45",[[tra_tainan,-12,tra_tainan+1,12]]]];
+tour_bus_99_vacation_time_return = [["10:55",[[stop_2,-5]]],["11:55",[[stop_2,-5]]],["12:55",[[stop_2,-5]]],["13:55",[[stop_2,-5]]],["14:55",[[stop_2,-5]]],["15:55",[[stop_2,-5]]],["16:55",[[stop_2,-5]]],["18:10",[[stop_2,-5]]]];
