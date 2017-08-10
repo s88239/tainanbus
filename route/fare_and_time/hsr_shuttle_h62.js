0a52a5056e1e6dcc746571dc270d39ec8c87b679
Eric Kuo <s88239@hotmail.com>
2017-08-10 20:54:23 +0800

    更新時刻表：H31高鐵快捷市府線(台南市政府：0530=>0515)、H62高鐵快捷奇美線(奇美醫院：0525=>0510)

--- route/fare_and_time/hsr_shuttle_h62.js
+++ route/fare_and_time/hsr_shuttle_h62.js
@@ -6,7 +6,7 @@ hsr_shuttle_h62_main_stop_time_consume = [0, 15, 22, 25, 35, 42, 45, 50, 65];
 hsr_shuttle_h62_important_stop = [0, 1, 3, 6, 8]; // 桂田酒店, 奇美醫院, 南紡購物中心, 仁德交流道, 高鐵臺南站
 var start_stop = 1; // 奇美醫院
 var end_stop = 8; // 高鐵臺南站
-hsr_shuttle_h62_time_go = [["05:25",start_stop,end_stop],["05:50",start_stop,end_stop],["06:25",start_stop,end_stop],["07:00",start_stop,end_stop],["07:25",start_stop,end_stop],["08:00",start_stop,end_stop],["08:25",start_stop,end_stop],
+hsr_shuttle_h62_time_go = [["05:10",start_stop,end_stop],["05:50",start_stop,end_stop],["06:25",start_stop,end_stop],["07:00",start_stop,end_stop],["07:25",start_stop,end_stop],["08:00",start_stop,end_stop],["08:25",start_stop,end_stop],
 ["08:45"],["09:25",start_stop,end_stop],["09:35",start_stop,end_stop],["10:00",start_stop,end_stop],["10:10"],["11:00",start_stop,end_stop],["11:10"],["12:00",start_stop,end_stop],["12:10",start_stop,end_stop],
 ["12:35",start_stop,end_stop],["12:45"],["13:10"],["13:55",start_stop,end_stop],["14:15",start_stop,end_stop],["14:25",start_stop,end_stop],["15:00",start_stop,end_stop],["15:20"],["15:45",start_stop,end_stop],["16:05",start_stop,end_stop],
 ["16:25",start_stop,end_stop],["17:00",start_stop,end_stop],["17:10"],["18:00",start_stop,end_stop],["18:10",start_stop,end_stop],["18:35",start_stop,end_stop],["19:00",start_stop,end_stop],["19:10"],["20:00",start_stop,end_stop],
