2808bd10b2925f4a09f6136806190a190a81ba58
Eric Kuo <s88239@hotmail.com>
2017-08-21 22:54:29 +0800

    更新時刻表：黃幹線、黃1、黃12、黃13、棕10

--- route/fare_and_time/brown_10.js
+++ route/fare_and_time/brown_10.js
@@ -22,5 +22,5 @@ brown_10_main_stop_name = ["學甲<br />(民權路)","學甲","港仔尾","一�
 brown_10_main_stop_time_consume = [0, 2, 25, 33, 40];
 brown_10_important_stop = [0, 1, 4]; // 學甲(民權路), 學甲, 麻豆國中
 var skip_stop = 2; // 港仔尾
-brown_10_time_go = [["06:40",[[3,4]]],["07:00",['L',skip_stop,[3,4,4,2]]],["09:10",['L',skip_stop]],["11:35",['L',skip_stop]],["14:35",['L',skip_stop]],["16:15",['L',skip_stop]],["16:25",[skip_stop]]];
-brown_10_time_return = [["07:40",[skip_stop]],["07:50",['L',skip_stop]],["10:00",['L',skip_stop]],["12:20",['L',[2,8,1,2]]],["15:20",['L',skip_stop]],["17:05",['L',skip_stop,[3,2,2,5]]],["17:15",[skip_stop,[3,2,2,3]]]];
+brown_10_time_go = [["06:40",[[3,8,4,2]]],["07:00",['L',skip_stop,[3,4,4,2]]],["09:10",['L',skip_stop]],["11:35",['L',skip_stop]],["14:35",['L',skip_stop]],["16:15",['L',skip_stop]],["16:25",[skip_stop]]];
+brown_10_time_return = [["07:40",[skip_stop]],["07:50",['L',skip_stop]],["10:00",['L',skip_stop]],["12:20",['L',skip_stop]],["15:20",['L',skip_stop]],["17:05",['L',skip_stop,[3,2,2,5]]],["17:15",[[3,2,2,8]]]];
--- route/fare_and_time/yellow.js
+++ route/fare_and_time/yellow.js
@@ -42,8 +42,8 @@ var skip_stop = 1; // 蘭花園區
 var chimei = 3; // 柳營奇美醫院
 yellow_time_go = [["06:00",['L']],["06:20",start_stop,hsinyin,['L']],["06:30",['L']],["06:45",start_stop,chimei],["07:15",start_stop,hsinyin],["07:30",['L']],
 ["08:00",start_stop,hsinyin],["08:30"],["08:50",start_stop,hsinyin,['L']],["09:30"],["10:00",start_stop,hsinyin],["10:30",['L']],["11:00",start_stop,hsinyin,['L']],["11:30",['L']],["12:00",start_stop,hsinyin],
-["12:30",['L']],["13:00",start_stop,hsinyin],["13:30",['L']],["14:00",start_stop,hsinyin,['L']],["14:30"],["15:00",start_stop,hsinyin,['L']],["15:30"],["16:00",start_stop,hsinyin],["16:30",['L']],
-["17:00",start_stop,hsinyin,['L']],["17:30",['<span style="color: red;">L</span>繞',]],["18:00",['L']],["18:15",start_stop,hsinyin],["18:30",start_stop,hsinyin],["19:00"],["20:00"]
+["12:30",['L']],["13:00",start_stop,hsinyin],["13:30",['L']],["14:00",start_stop,hsinyin,['L']],["14:30"],["15:00",start_stop,hsinyin,['L']],["15:30"],["16:00",start_stop,hsinyin,['繞']],["16:30",['<span style="color: red;">L</span>繞']],
+["17:00",start_stop,hsinyin,['<span style="color: red;">L</span>繞']],["17:30",['<span style="color: red;">L</span>繞',]],["18:00",['L']],["18:15",start_stop,hsinyin],["18:30",start_stop,hsinyin],["19:00"],["20:00"]
 ];
 yellow_time_return = [["06:10",hsinyin,start_stop],["06:35",hsinyin,start_stop,['L']],["06:50",hsinyin,start_stop],["06:00",['繞']],["06:30",[[hsinyin,5]]],["07:55",hsinyin,start_stop],
 ["07:30",['L',[hsinyin,5]]],["08:55",hsinyin,start_stop],["08:30",['L',[hsinyin,5]]],["09:55",hsinyin,start_stop,['L']],["09:30",['L',[hsinyin,5]]],["10:55",hsinyin,start_stop],["10:30",['L',[hsinyin,5]]],["11:55",hsinyin,start_stop],["11:30",[[hsinyin,5]]],["13:00",hsinyin,start_stop,['L']],
