84409b997111d703f47a5f5e6b0a8307af6a2fbb
Eric Kuo <s88239@hotmail.com>
2017-10-02 19:18:50 +0800

    新增站位：黃12、黃12-2、黃13(新增「麒麟隧道口」)、黃15(新增「賞蓮大道南站」、「賞蓮大道北站」)

--- route/fare_and_time/yellow_15.js
+++ route/fare_and_time/yellow_15.js
@@ -1,11 +1,11 @@
 yellow_15_interval_name = ["白河","大排竹","玉豐","埤肚","枋子林","蓮潭","詔安厝"];
 yellow_15_interval_stop = [
 ["白河站","白河農會"], // 白河
-["大排竹","北大排竹"], // 大排竹
-["南玉豐","玉豐"], // 玉豐
+["賞蓮大道南站","賞蓮大道北站","大排竹","北大排竹"], // 大排竹
+["南玉豐","玉豐","蓮花公園"], // 玉豐
 ["埤肚"], // 埤肚
-["枋子林"], // 枋子林
-["廣安里","蓮潭"], // 蓮潭
+["枋子林","廣安里"], // 枋子林
+["蓮潭"], // 蓮潭
 ["詔安厝(一)","詔安厝(二)"] // 詔安厝
 ];
 yellow_15_fare = [
@@ -20,8 +20,8 @@ yellow_15_fare = [
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-yellow_15_main_stop_name = ["白河","玉豐","枋子林","詔安厝"];
-yellow_15_main_stop_time_consume = [0, 8, 11, 15];
-yellow_15_important_stop = [0, 3]; // 白河, 三層崎
-yellow_15_time_go = [["06:45"],["10:50"],["18:35"]];
-yellow_15_time_return = [["06:25"],["07:05"],["11:10"]];
+yellow_15_main_stop_name = ["白河","賞蓮大道<br />南站","玉豐","枋子林","詔安厝<br />(二)"];
+yellow_15_main_stop_time_consume = [0, 5, 8, 11, 15];
+yellow_15_important_stop = [0, 4]; // 白河, 詔安厝(二)
+yellow_15_time_go = [["06:45"],["10:50",['L']],["18:35",['L']]];
+yellow_15_time_return = [["06:25"],["07:05"],["11:10",['L']]];
