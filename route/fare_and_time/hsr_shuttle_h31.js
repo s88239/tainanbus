0ee774297987bdad09c9d7b849047fe45b3bf933
Eric Kuo <s88239@hotmail.com>
2017-10-02 14:26:09 +0800

    更新時刻表：高鐵快捷公車H31、H62(增班1往返於頭班車)

--- route/fare_and_time/hsr_shuttle_h31.js
+++ route/fare_and_time/hsr_shuttle_h31.js
@@ -6,7 +6,7 @@ hsr_shuttle_h31_main_stop_time_consume = [0, 10, 17, 25, 30, 40, 45, 60];
 hsr_shuttle_h31_important_stop = [0, 1, 5, 6, 7]; // 原住民文化會館, 臺南市政府, 臺南航空站(奇美幸福工廠), 奇美博物館, 高鐵臺南站
 var start_stop = 1; // 臺南市政府
 var end_stop = 7; // 高鐵臺南站
-hsr_shuttle_h31_time_go = [["05:15",start_stop,end_stop],["06:00",start_stop,end_stop],["06:40",start_stop,end_stop],["07:00",start_stop,end_stop],["07:35",start_stop,end_stop],["07:50",start_stop,end_stop],["08:20",start_stop,end_stop],
+hsr_shuttle_h31_time_go = [["05:10",start_stop,end_stop],["05:30",start_stop,end_stop],["06:00",start_stop,end_stop],["06:40",start_stop,end_stop],["07:00",start_stop,end_stop],["07:35",start_stop,end_stop],["07:50",start_stop,end_stop],["08:20",start_stop,end_stop],
 ["08:35",start_stop,end_stop],["08:50",start_stop,end_stop],["09:20",start_stop,end_stop],["09:35",start_stop,end_stop],["09:40"],["10:20",start_stop,end_stop],["10:35",start_stop,end_stop],["10:40"],["11:20",start_stop,end_stop],
 ["11:35",start_stop,end_stop],["11:40"],["12:20",start_stop,end_stop],["12:35",start_stop,end_stop],["12:40"],["13:20",start_stop,end_stop],["13:35",start_stop,end_stop],["13:40"],["14:20",start_stop,end_stop],["14:35",start_stop,end_stop],
 ["14:40"],["15:20",start_stop,end_stop],["15:35",start_stop,end_stop],["15:40"],["16:20",start_stop,end_stop],["16:35",start_stop,end_stop],["16:40"],["17:20",start_stop,end_stop],["17:35",start_stop,end_stop],["17:40"],
@@ -19,7 +19,7 @@ hsr_shuttle_h31_main_stop_time_consume_return = [0, 15, 20, 30, 35, 43, 50, 60];
 hsr_shuttle_h31_important_stop_return = [0, 1, 2, 6, 7]; // 高鐵臺南站, 奇美博物館, 臺南航空站(奇美幸福工廠), 臺南市政府, 原住民文化會館
 var start_stop_return = 0; // 高鐵臺南站
 var end_stop_return = 6; // 臺南市政府
-hsr_shuttle_h31_time_return = [["07:00",start_stop_return,end_stop_return],["07:35",start_stop_return,end_stop_return],["07:50",start_stop_return,end_stop_return],["08:20",start_stop_return,end_stop_return],["08:35",start_stop_return,end_stop_return],["08:50"],["09:20",start_stop_return,end_stop_return],
+hsr_shuttle_h31_time_return = [["06:35",start_stop_return,end_stop_return],["07:00",start_stop_return,end_stop_return],["07:35",start_stop_return,end_stop_return],["07:50",start_stop_return,end_stop_return],["08:20",start_stop_return,end_stop_return],["08:35",start_stop_return,end_stop_return],["08:50"],["09:20",start_stop_return,end_stop_return],
 ["09:35",start_stop_return,end_stop_return],["09:50"],["10:20",start_stop_return,end_stop_return],["10:35",start_stop_return,end_stop_return],["10:50"],["11:20",start_stop_return,end_stop_return],["11:35",start_stop_return,end_stop_return],["11:50"],["12:20",start_stop_return,end_stop_return],["12:35",start_stop_return,end_stop_return],
 ["12:50"],["13:20",start_stop_return,end_stop_return],["13:35",start_stop_return,end_stop_return],["13:50"],["14:20",start_stop_return,end_stop_return],["14:35",start_stop_return,end_stop_return],["14:50"],["15:20",start_stop_return,end_stop_return],["15:35",start_stop_return,end_stop_return],
 ["15:50"],["16:20",start_stop_return,end_stop_return],["16:35",start_stop_return,end_stop_return],["16:50"],["17:20",start_stop_return,end_stop_return],["17:35",start_stop_return,end_stop_return],["17:50"],["18:20",start_stop_return,end_stop_return],["18:35",start_stop_return,end_stop_return],["18:50"],
