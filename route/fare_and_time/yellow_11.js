de0451ba16a9390a5827ab305e47d3cac8390ba3
Eric Kuo <s88239@hotmail.com>
2017-11-10 17:07:47 +0800

    更新時刻表：黃11

--- route/fare_and_time/yellow_11.js
+++ route/fare_and_time/yellow_11.js
@@ -27,7 +27,7 @@ yellow_11_fare = [
 // [time, other] or
 // [time, start_stop, end_stop, other]
 yellow_11_main_stop_name = ["白河商工","白河","東山","嶺南里","宅子內","斑花橋","東原","青山","科尾"];
-yellow_11_main_stop_time_consume = [0, 2, 7, 27, 29, 18, 24, 29, 32];
+yellow_11_main_stop_time_consume = [0, 5, 10, 30, 32, 21, 27, 32, 35];
 yellow_11_important_stop = [0, 1, 2, 7]; // 白河商工, 白河, 東山, 青山
 var start_stop = 1; // 白河
 var start_stop_2 = 0; // 白河商工
@@ -35,5 +35,5 @@ var end_stop = 7; // 青山
 var end_stop_2 = 8; // 科尾
 var stop_1 = 3; // 嶺南里
 var stop_2 = 4; // 宅子內
-yellow_11_time_go = [["06:00",start_stop,end_stop,[stop_1,stop_2]],["06:05",start_stop,end_stop,[[stop_2+1,13]]],["07:20",start_stop,end_stop,[stop_1,stop_2]],["09:10",start_stop,end_stop,[stop_1,stop_2]],["09:30",start_stop,end_stop_2,[[stop_2+1,13]]],["10:40",start_stop,end_stop,[[stop_2+1,13]]],["12:00",start_stop,end_stop,[stop_1,stop_2]],["13:40",start_stop,end_stop,[stop_1,stop_2]],["15:00",start_stop,end_stop,[stop_1,stop_2]],["16:18",start_stop_2,end_stop,[stop_1,stop_2]],["17:13",start_stop_2,end_stop,[stop_1,stop_2]],["17:40",start_stop,end_stop,[[stop_2+1,13]]],["18:15",start_stop,end_stop,[stop_1,stop_2]]];
-yellow_11_time_return = [["06:35",end_stop,start_stop,[stop_1,stop_2]],["06:50",end_stop,start_stop_2,[[stop_2,13]]],["07:50",end_stop,start_stop,[stop_1,stop_2]],["09:40",end_stop,start_stop,[stop_1,stop_2]],["10:20",end_stop_2,start_stop,[[stop_2,13]]],["11:25",end_stop,start_stop,[[stop_2,13]]],["12:30",end_stop,start_stop,[stop_1,stop_2]],["14:10",end_stop,start_stop,[stop_1,stop_2]],["15:30",end_stop,start_stop,[stop_1,stop_2]],["16:55",end_stop,start_stop,[[stop_2,13]]],["17:50",end_stop,start_stop,[stop_1,stop_2]],["18:25",end_stop,start_stop,[stop_1,stop_2]],["18:50",end_stop,start_stop,[stop_1,stop_2]]];
+yellow_11_time_go = [["06:00",start_stop,end_stop,[stop_1,stop_2]],["06:05",start_stop,end_stop,[[stop_2+1,13]]],["07:20",start_stop,end_stop,[stop_1,stop_2]],["09:10",start_stop,end_stop,[stop_1,stop_2]],["09:30",start_stop,end_stop_2,[[stop_2+1,13]]],["10:40",start_stop,end_stop,[[stop_2+1,13]]],["12:00",start_stop,end_stop,[stop_1,stop_2]],["13:40",start_stop,end_stop,[stop_1,stop_2]],["15:00",start_stop,end_stop,[stop_1,stop_2]],["16:15",start_stop_2,end_stop,[stop_1,stop_2]],["17:15",start_stop_2,end_stop,[stop_1,stop_2]],["17:50",start_stop,end_stop,[[stop_2+1,13]]],["18:25",start_stop,end_stop,[stop_1,stop_2]]];
+yellow_11_time_return = [["06:35",end_stop,start_stop,[stop_1,stop_2]],["06:50",end_stop,start_stop_2,[[stop_2,13]]],["07:50",end_stop,start_stop,[stop_1,stop_2]],["09:40",end_stop,start_stop,[stop_1,stop_2]],["10:20",end_stop_2,start_stop,[[stop_2,13]]],["11:25",end_stop,start_stop,[[stop_2,13]]],["12:30",end_stop,start_stop,[stop_1,stop_2]],["14:10",end_stop,start_stop,[stop_1,stop_2]],["15:30",end_stop,start_stop,[stop_1,stop_2]],["16:55",end_stop,start_stop,[[stop_2,13]]],["17:55",end_stop,start_stop,[stop_1,stop_2]],["18:35",end_stop,start_stop,[stop_1,stop_2]],["19:00",end_stop,start_stop,[stop_1,stop_2]]];
