caa88819b4002257e5d4b05dc40d42312d1674c8
Eric Kuo <s88239@hotmail.com>
2018-06-01 08:41:31 +0800

    更新時刻表：紅幹線、綠5、綠6

--- route/fare_and_time/red.js
+++ route/fare_and_time/red.js
@@ -28,23 +28,23 @@ start_stop = 0;
 stop_1 = 2; // 臺南火車站
 stop_2 = 4; // 歸仁公所
 stop_3 = 6; // 關廟轉運站
-red_time_go = [["05:25",start_stop,stop_2,['W']],["05:45",start_stop,stop_3],["06:00",start_stop,stop_3,['繞',[stop_3,5]]],
-["06:15",start_stop,stop_3],["06:30"],["07:00",start_stop,stop_3],["07:20"],["07:40",start_stop,stop_3],
+red_time_go = [["05:25",start_stop,stop_2,['W']],["05:45",start_stop,stop_3],["05:55",start_stop,stop_3,['繞',[stop_3,5]]],
+["06:10",start_stop,stop_3],["06:30"],["07:00",start_stop,stop_3],["07:20"],["07:40",start_stop,stop_3],
 ["08:00"],["08:30"],["09:00",start_stop,stop_3],["09:30"],["10:00",start_stop,stop_3],["10:30"],["11:00",start_stop,stop_3],
 ["11:30"],["11:45",start_stop,stop_3],["12:00",start_stop,stop_3],["12:15",start_stop,stop_3],["12:30"],
 ["13:00",start_stop,stop_3],["13:30"],["14:00",start_stop,stop_3],["14:30"],["15:00",start_stop,stop_3],["15:30"],
 ["15:45",start_stop,stop_3],["16:00"],["16:15",start_stop,stop_3],["16:45",start_stop,stop_3],["16:50",start_stop,stop_3],
 ["17:00",start_stop,stop_3],["17:15",start_stop,stop_3],["17:30"],["17:55",start_stop,stop_3],["18:15",start_stop,stop_3],
 ["18:45"],["19:10"],["19:30",start_stop,stop_3],["20:00"],["20:30",start_stop,stop_3],
-["21:00",start_stop,stop_3],["21:15",start_stop,stop_3],["21:30"],["22:00",start_stop,stop_3]
+["21:00",start_stop,stop_3],["21:15",start_stop,stop_3],["21:30"],["22:10",start_stop,stop_3]
 ];
 red_time_return = [["05:50"],["06:10",stop_3,start_stop],["06:20",stop_3,start_stop],["06:30",stop_2,start_stop,['W']],["06:20"],
 ["06:40",stop_3,start_stop],["06:50"],["07:00",stop_3,start_stop],["07:20",stop_3,start_stop],["07:30",stop_3,start_stop],
 ["07:55"],["08:30",stop_3,start_stop],["09:00",stop_3,start_stop],["09:25"],["09:50"],["10:25"],["10:55",stop_3,start_stop],
-["11:25"],["11:55",stop_3,start_stop],["12:10"],["12:45",stop_3,start_stop],["12:50"],
+["11:25"],["11:55",stop_3,start_stop],["12:10"],["12:40",stop_3,start_stop],["12:55"],
 ["13:30",stop_3,start_stop],["13:55",stop_3,start_stop],["14:15",stop_3,start_stop],["14:25"],["14:55",stop_3,start_stop],
 ["15:25"],["16:00",stop_3,start_stop],["16:30",stop_3,start_stop],["16:50",['繞',[stop_2,5]]],
 ["17:10",['繞',[stop_2,5]]],["17:30",stop_3,start_stop],["17:40"],["17:50"],
 ["18:30",stop_3,start_stop],["18:45",stop_3,start_stop],["18:55",stop_3,start_stop],["19:25"],["19:55",stop_3,start_stop],
-["20:20"],["20:45",stop_3,start_stop],["20:50"],["21:20"],["22:00",stop_3,start_stop]
+["20:20"],["20:50",stop_3,start_stop],["20:50"],["21:25"],["22:00",stop_3,start_stop]
 ];
