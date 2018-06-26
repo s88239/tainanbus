42d6fdf6b8d2f3ef705434fd8f4d470a19b52a74
Eric Kuo <s88239@hotmail.com>
2018-06-26 23:45:21 +0800

    更新時刻表：南科紅線、南科藍線、南科綠線

--- route/fare_and_time/STSP_blue.js
+++ route/fare_and_time/STSP_blue.js
@@ -12,7 +12,7 @@ var skip_list_3 = [...skip_list_1, ...skip_list_2];
 var stop_1 = 3; // 民生水塔
 var TRA_NK = 7;
 STSP_blue_time_go = [["06:20",skip_list_3],["06:40",skip_list_3],["06:57",[...skip_list_2,[stop_1,8]]],["07:12",[...skip_list_2,[stop_1,8,TRA_NK+2,1,TRA_NK+3,1]]],["07:16",[...skip_list_2,[stop_1,8,TRA_NK+2,1,TRA_NK+3,1]]],
-["07:38",[...skip_list_1,[TRA_NK,11]]],["07:56",[...skip_list_3]],["07:58",[...skip_list_3]],["08:19",[...skip_list_3,[TRA_NK+1,1]]],["08:25",[...skip_list_3]],["09:09",skip_list_3],
+["07:38",[...skip_list_1,[TRA_NK,11]]],["07:56",[...skip_list_3]],["08:03",[...skip_list_3]],["08:19",[...skip_list_3,[TRA_NK+1,1]]],["08:25",[...skip_list_3]],["09:09",skip_list_3],
 ["09:30",skip_list_3],["10:34",skip_list_3],["11:24",skip_list_3],["12:11",skip_list_3],["13:30",skip_list_3],
 ["14:39",skip_list_3],["15:15",skip_list_3],["16:01",[skip_stop,...skip_list_2,[skip_stop_2,-3,stop_1,8]]],["16:35",skip_list_3],["17:00",0,TRA_NK,[skip_stop,...skip_list_2,[skip_stop_2,-3,stop_1,8,TRA_NK,1]]],
 ["17:00",0,TRA_NK,[skip_stop,...skip_list_2,[skip_stop_2,-3,stop_1,8,TRA_NK,1]]],["17:18",skip_stop_2,TRA_NK,[...skip_list_2,[stop_1,8]]],["17:18",skip_stop_2,TRA_NK,[...skip_list_2,[stop_1,8]]]
@@ -24,5 +24,5 @@ STSP_blue_main_stop_time_consume_return = [0, 2, 4, 7, 8, 11, 13, 17, 23, 25, 26
 STSP_blue_important_stop_return = [0, 3, 5, 7]; // Park 17商場, 瀚宇彩晶, 華園宿舍, 臺鐵南科站
 var LM_list = [8, 9, 10];
 var return_stop_1 = 11;
-STSP_blue_time_return = [["17:15",LM_list],["17:35",LM_list],["17:40",[[return_stop_1,11]]],["17:50",LM_list],["18:03",LM_list],["18:20",LM_list],["18:27",[[return_stop_1,11]]],["18:57",LM_list],["19:17",LM_list],["19:44",LM_list],
+STSP_blue_time_return = [["17:15",LM_list],["17:35",LM_list],["17:40",[[return_stop_1,11]]],["17:50",LM_list],["18:03",LM_list],["18:20",LM_list],["18:27",LM_list],["18:57",LM_list],["19:17",LM_list],["19:44",LM_list],
 ["20:17",LM_list],["20:44",LM_list],["21:16",LM_list]];
--- route/fare_and_time/STSP_green.js
+++ route/fare_and_time/STSP_green.js
@@ -5,11 +5,11 @@ STSP_green_main_stop_name = ["Park 17<br />商場","育成<br />中心","聯電"
 STSP_green_main_stop_time_consume = [0, 2, 3, 5, 38];
 STSP_green_important_stop = [0, 4]; // Park 17商場, 高鐵臺南站
 var hsr_tainan_go = 4;
-STSP_green_time_go = [["05:50"],["07:20"],["08:20"],["09:20"],["10:10",['綠',[hsr_tainan_go,12]]],["11:50"],["13:20"],["14:50"],["15:50"],["16:50"],["17:50"],["18:50"]];
+STSP_green_time_go = [["05:50"],["07:00"],["08:20"],["09:20"],["10:10",['綠',[hsr_tainan_go,12]]],["11:50"],["13:20"],["14:50"],["15:50"],["16:50"],["17:45"],["18:50"]];
 
 // The name of return stops are different
 STSP_green_main_stop_name_return = ["高鐵<br />臺南站","群創<br />D廠","瀚宇<br />彩晶","動物<br />中心","Park 17<br />商場"];
 STSP_green_main_stop_time_consume_return = [0, 33, 35, 37, 39];
 STSP_green_important_stop_return = [0, 4]; // 高鐵臺南站, Park 17商場
 var hsr_tainan_return = 0;
-STSP_green_time_return = [["07:05"],["08:20"],["09:30"],["10:25"],["11:25",['綠',[hsr_tainan_return+1,12]]],["12:50"],["14:25"],["15:50"],["16:50"],["17:50"],["18:50"],["19:50"]];
+STSP_green_time_return = [["07:05"],["08:18"],["09:30"],["10:25"],["11:25",['綠',[hsr_tainan_return+1,12]]],["12:50"],["14:25"],["15:50"],["16:50"],["17:50"],["18:50"],["19:50"]];
