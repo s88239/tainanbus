3dca588e4e5fe02bb3998770727e98203178afe4
Eric Kuo <s88239@hotmail.com>
2018-06-28 22:50:54 +0800

    站位更名：11路、藍24、藍25(聖母廟加註路名、文衡殿更名為篤加里)

--- route/fare_and_time/city_bus_11.js
+++ route/fare_and_time/city_bus_11.js
@@ -1,9 +1,9 @@
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-city_bus_11_main_stop_name = ["大成<br />路口","臺南站","小西門<br />(大億麗緻)","臺南<br />火車站","民德<br />路口","花園夜市<br />(和緯路)","安中路<br />一段","海東<br />國小","土城<br />中學","聖母廟","城西里"];
+city_bus_11_main_stop_name = ["大成<br />路口","臺南站","小西門<br />(大億麗緻)","臺南<br />火車站","民德<br />路口","花園夜市<br />(和緯路)","安中路<br />一段","海東<br />國小","土城<br />中學","聖母廟<br />(城安路)","城西里"];
 city_bus_11_main_stop_time_consume = [0, 2, 5, 17, 22, 27, 37, 47, 54, 61, 67];
-city_bus_11_important_stop = [0, 1, 3, 5, 7, 9, 10]; // 大成路口, 臺南站, 臺南火車站, 花園夜市(和緯路), 海東國小, 聖母廟, 城西里
+city_bus_11_important_stop = [0, 1, 3, 5, 7, 9, 10]; // 大成路口, 臺南站, 臺南火車站, 花園夜市(和緯路), 海東國小, 聖母廟(城安路), 城西里
 var start_stop = 10; // 城西里
 var tainan_railway_station = 3; // 臺南火車站
 var stop_1 = 7; // 海東國小
