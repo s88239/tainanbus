49f01eedf418befda940fb267bbeedca7515a4bd
Eric Kuo <s88239@hotmail.com>
2018-07-01 23:26:42 +0800

    更新時刻表及路線圖：雙層巴士西環線(增加經花園夜市之星光班次)

--- route/fare_and_time/sightseeing_western.js
+++ route/fare_and_time/sightseeing_western.js
@@ -4,4 +4,9 @@
 sightseeing_western_main_stop_name = ["臺南火車站<br />(北站)","赤崁樓","安平<br />漁人碼頭","億載金城","市政中心<br />(永華路)","小西門<br />(大億麗緻)","孔廟","臺南火車站<br />(北站)"];
 sightseeing_western_main_stop_time_consume = [0, 8, 33, 36, 45, 53, 57, 63];
 sightseeing_western_important_stop = [0, 2, 7]; // 臺南火車站(北站), 安平漁人碼頭, 臺南火車站(北站)
-sightseeing_western_time_go = [["08:30"],["09:00"],["10:30"],["11:00"],["12:00"],["13:30"],["14:00"],["15:00"],["16:30"],["17:00"],["18:00"],["18:30"]];
+sightseeing_western_time_go = [["09:00"],["10:30",["導覽"]],["11:00",["導覽"]],["14:00",["導覽"]],["15:00",["導覽"]],["16:30",["導覽"]],["17:00"],["18:00"]];
+
+sightseeing_western_main_stop_name_night = ["臺南火車站<br />(北站)","赤崁樓","小北商場","花園夜市","小西門<br />(大億麗緻)","孔廟","臺南火車站<br />(北站)"];
+sightseeing_western_main_stop_time_consume_night = [0, 8, 15, 20, 30, 34, 50];
+sightseeing_western_important_stop_night = [0, 3, 6]; // 臺南火車站(北站), 花園夜市, 臺南火車站(北站)
+sightseeing_western_time_go_night = [["18:30",["導覽"]],["19:30",["導覽"]],["20:00"],["20:30"],["21:00"]];
