cdfbfa8cfad6f90d702c31edfabfeb10cb97fb10
Eric Kuo <s88239@hotmail.com>
2019-01-28 22:42:13 +0800

    更新時刻表：雙層巴士西環線、東環線

--- route/fare_and_time/sightseeing_eastern.js
+++ route/fare_and_time/sightseeing_eastern.js
@@ -4,4 +4,4 @@
 sightseeing_eastern_main_stop_name = ["臺南火車站<br />(北站)","香格里拉<br />飯店","南紡<br />購物中心","巴克禮<br />公園","孔廟","臺南火車站<br />(北站)"];
 sightseeing_eastern_main_stop_time_consume = [0, 3, 16, 24, 35, 40];
 sightseeing_eastern_important_stop = [0, 2, 5]; // 臺南火車站(北站), 南紡購物中心, 臺南火車站(北站)
-sightseeing_eastern_time_go = [["09:55"],["12:55"],["15:55"]];
+sightseeing_eastern_time_go = [["09:30"],["12:30"],["15:30"]];
--- route/fare_and_time/sightseeing_western.js
+++ route/fare_and_time/sightseeing_western.js
@@ -4,9 +4,9 @@
 sightseeing_western_main_stop_name = ["臺南火車站<br />(北站)","赤崁樓","神農街","安平<br />漁人碼頭","億載金城","市政中心<br />(永華路)","小西門<br />(大億麗緻)","孔廟","臺南火車站<br />(北站)"];
 sightseeing_western_main_stop_time_consume = [0, 9, 11, 27, 31, 35, 42, 51, 58];
 sightseeing_western_important_stop = [0, 3, 8]; // 臺南火車站(北站), 安平漁人碼頭, 臺南火車站(北站)
-sightseeing_western_time_go = [["09:00"],["10:30",["導覽"]],["11:00",["導覽"]],["14:00",["導覽"]],["15:00",["導覽"]],["16:30",["導覽"]],["17:00"],["18:00"]];
+sightseeing_western_time_go = [["09:00"],["10:30"],["11:00"],["12:00"],["14:00"],["15:00"],["16:30"],["17:00"],["18:00"]];
 
 sightseeing_western_main_stop_name_night = ["臺南火車站<br />(北站)","赤崁樓","小北商場","花園夜市","神農街","小西門<br />(大億麗緻)","孔廟","臺南火車站<br />(北站)"];
 sightseeing_western_main_stop_time_consume_night = [0, 8, 12, 16, 20, 29, 38, 45];
 sightseeing_western_important_stop_night = [0, 3, 7]; // 臺南火車站(北站), 花園夜市, 臺南火車站(北站)
-sightseeing_western_time_go_night = [["18:30",["導覽"]],["19:30",["導覽"]],["20:00"],["20:30"],["21:00"]];
+sightseeing_western_time_go_night = [["18:20"],["19:00"],["19:40"]];
