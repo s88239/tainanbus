7014f18e4c25443ea92f0fac43c6dc4e2f8f12ce
Eric Kuo <s88239@hotmail.com>
2019-01-30 23:42:26 +0800

    更新路線圖及時刻表：雙層巴士東環線、西環線

--- route/fare_and_time/sightseeing_eastern.js
+++ route/fare_and_time/sightseeing_eastern.js
@@ -1,7 +1,7 @@
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-sightseeing_eastern_main_stop_name = ["臺南火車站<br />(北站)","香格里拉<br />飯店","南紡<br />購物中心","巴克禮<br />公園","孔廟","臺南火車站<br />(北站)"];
-sightseeing_eastern_main_stop_time_consume = [0, 3, 16, 24, 35, 40];
-sightseeing_eastern_important_stop = [0, 2, 5]; // 臺南火車站(北站), 南紡購物中心, 臺南火車站(北站)
+sightseeing_eastern_main_stop_name = ["臺南火車站<br />(北站)","香格里拉<br />飯店","巴克禮<br />公園","臺南航空站<br />(奇美幸福工廠)","奇美博物館","孔廟","臺南火車站<br />(北站)"];
+sightseeing_eastern_main_stop_time_consume = [0, 3, 16, 22, 26, 40, 45];
+sightseeing_eastern_important_stop = [0, 4, 6]; // 臺南火車站(北站), 奇美博物館, 臺南火車站(北站)
 sightseeing_eastern_time_go = [["09:30"],["12:30"],["15:30"]];
