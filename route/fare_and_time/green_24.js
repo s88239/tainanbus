452f3f1cafdc9a679710c0e4bab1f35b16ec2f38
Eric Kuo <s88239@hotmail.com>
2019-03-07 21:55:45 +0800

    更新時刻表：綠24、藍2、黃5； 更新路線圖：綠24(繞駛南區水資源局)、藍2(繞駛蘆竹溝)、黃5(繞駛吳晉淮音樂紀念館)； 更新票價表：黃5

--- route/fare_and_time/green_24.js
+++ route/fare_and_time/green_24.js
@@ -3,7 +3,7 @@ green_24_interval_stop = [
 ["玉井站","玉安街口","玉田里"], // 玉井
 ["後旦","竹圍","鹿陶廟","鹿陶","埔頭子","大林路口","鹿陶洋","東西煙"], // 鹿陶洋
 ["油車","楠西區圖書館","楠西區公所","楠西街","楠西","楠西國中","水庫路口"], // 楠西
-["照興里","小普陀山","興北","樂群新村","曾文二橋","曾文工務段","曾文青年活動中心","曾文管理局"], // 曾文
+["照興里","小普陀山","興北","樂群新村","曾文二橋","曾文工務段","曾文青年活動中心","南區水資源局","曾文管理局"], // 曾文
 ["東口遊憩區","溪畔遊樂區","曾文之眼","壩頂","曾文遊樂區","觀景樓"] // 曾文水庫
 ];
 green_24_fare = [
@@ -16,8 +16,10 @@ green_24_fare = [
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-green_24_main_stop_name = ["玉井","鹿陶","鹿陶洋","楠西","興北","曾文<br />管理局","觀景樓"];
+green_24_main_stop_name = ["玉井站","鹿陶","鹿陶洋","楠西","興北","曾文<br />管理局","觀景樓"];
 green_24_main_stop_time_consume = [0, 7, 9, 15, 20, 25, 40];
-green_24_important_stop = [0, 3, 5, 6]; // 玉井, 楠西, 曾文管理局, 觀景樓
-green_24_time_go = [["07:20",0,5],["09:00",0,5],["09:30"],["10:35",0,5],["12:15",['W']],["14:00"],["15:05",0,5],["16:55",0,5],["17:20",['H']]];
-green_24_time_return = [["07:45",5,0],["09:25",5,0],["10:10"],["11:00",5,0],["12:55",['W']],["14:40"],["15:30",5,0],["17:20",5,0],["18:00",['H']]];
+green_24_important_stop = [0, 3, 5, 6]; // 玉井站, 楠西, 曾文管理局, 觀景樓
+var Yujing = 0; // 玉井站
+var Zengwun_Administration = 5; // 曾文管理局
+green_24_time_go = [["07:20",Yujing,Zengwun_Administration,['繞',[Zengwun_Administration,5]]],["09:00",Yujing,Zengwun_Administration],["09:30"],["10:35",Yujing,Zengwun_Administration],["11:40",Yujing,Zengwun_Administration],["12:15",['W']],["14:00"],["15:05",Yujing,Zengwun_Administration],["16:55",Yujing,Zengwun_Administration],["17:20",['H']]];
+green_24_time_return = [["07:50",Zengwun_Administration,Yujing],["09:25",Zengwun_Administration,Yujing],["10:10"],["11:00",Zengwun_Administration,Yujing],["12:05",Zengwun_Administration,Yujing],["12:55",['W']],["14:40"],["15:30",Zengwun_Administration,Yujing],["17:20",Zengwun_Administration,Yujing],["18:00",['H']]];
--- route/fare_and_time/yellow_5.js
+++ route/fare_and_time/yellow_5.js
@@ -3,7 +3,7 @@ yellow_5_interval_stop = [ // 2018.11.18 checked
 ["新營站","新營鐵道地景公園","中營興業里","中山路","第一市場","真武殿","南光中學"], // 新營
 ["副產","實驗所"], // 副產
 ["柳營奇美醫院","鳳和中學","柳營火車站","柳營代天院"], // 柳營火車站
-["劉家古厝","八翁里","八老爺"], // 八老爺
+["劉家古厝","吳晉淮音樂紀念館","八翁里","八老爺"], // 八老爺
 ["賀建里","十六甲","中庄"], // 中庄
 ["紅毛厝","顏姓祖厝","下營區公所","下營廟前"] // 下營
 ];
@@ -11,19 +11,19 @@ yellow_5_fare = [
 [26],
 [26,26],
 [26,26,26],
-[28,26,26,26],
+[27,26,26,26],
 [42,33,26,26,26],
-[48,39,30,26,26,26]
+[51,42,32,26,26,26]
 ];
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-yellow_5_main_stop_name = ["新營站","柳營<br />火車站","八老爺","紅毛厝","下營<br />區公所","下營廟前"];
-yellow_5_main_stop_time_consume = [0, 14, 18, 27, 29, 30];
-yellow_5_important_stop = [0, 1, 5]; // 新營站, 柳營火車站, 下營廟前
+yellow_5_main_stop_name = ["新營站","柳營<br />火車站","吳晉淮音<br />樂紀念館","八老爺","紅毛厝","下營<br />區公所","下營廟前"];
+yellow_5_main_stop_time_consume = [0, 14, 20, 24, 33, 35, 36];
+yellow_5_important_stop = [0, 1, 6]; // 新營站, 柳營火車站, 下營廟前
 var Sinying = 0; // 新營站
 var TRA_Liouying = 1; // 柳營火車站
-var Balaoye = 2; // 八老爺
-var Hongmaocuo = 3; // 紅毛厝
-yellow_5_time_go = [["08:20"],["09:30",['L']],["12:15"],["16:00",['L']],["17:15",['H']]];
-yellow_5_time_return = [["09:00",[[Hongmaocuo,1,Sinying,-1]]],["12:30",['L',[Hongmaocuo,1,TRA_Liouying,1,Sinying,-2]]],["13:00",[[Hongmaocuo,1,Balaoye,-1]]],["16:35",['L',[Hongmaocuo,1,TRA_Liouying,1,Sinying,3]]],["18:00",['H',[Hongmaocuo,1,Balaoye,-2,Sinying,1]]]];
+var Balaoye = 3; // 八老爺
+var Hongmaocuo = 4; // 紅毛厝
+yellow_5_time_go = [["08:20"],["09:30",['L']],["12:10"],["15:50",['L']],["17:15",['H']]];
+yellow_5_time_return = [["09:05",[[Hongmaocuo,1,Sinying,-2]]],["12:30",['L',[Hongmaocuo,1,Sinying,-2]]],["12:55",[[Hongmaocuo,1,Balaoye,-1,Sinying,-1]]],["16:35",['L',[Hongmaocuo,1,Sinying,3]]],["18:00",['H',[Hongmaocuo,1,Balaoye,-2]]]];
