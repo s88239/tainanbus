8d50af0d703a857a693d1ff48fdeaa7bae50c040
Eric Kuo <s88239@hotmail.com>
2019-07-29 23:07:31 +0800

    更新時刻表：黃5、黃11-2； 更新票價表：棕1； 新增站位：8041C(增「臺鐵正義站」)、黃11-2(增「岩坑」、「橫路」、「田寮子」)； 裁撤站位：黃5(裁撤「吳晉淮音樂紀念館」)

--- route/fare_and_time/yellow_11_2.js
+++ route/fare_and_time/yellow_11_2.js
@@ -7,9 +7,9 @@ yellow_11_2_interval_stop = [
 ["番子嶺","開門橋","萬應公廟","番子厝"], // 番子嶺
 ["斑芝花坑","斑花橋","柿子寮"], // 斑芝花坑
 ["三塊厝","嶺南里路口","西東原","東原派出所","雙溪庄"], // 西東原
-["北大洋","大洋","後坑頭","南勢里活動中心"], // 北大洋
-["南勢庄"], // 南勢庄
-["碧蓮寺"], // 碧蓮寺
+["北大洋","田寮子","大洋","後坑頭","南勢里活動中心"], // 北大洋
+["南勢庄","岩坑"], // 南勢庄
+["碧蓮寺","橫路"], // 碧蓮寺
 ["橫路休息站"] // 橫路休息站
 ];
 yellow_11_2_fare = [
@@ -26,15 +26,15 @@ yellow_11_2_fare = [
 [74,63,56,53,50,40,31,26,26,26,26]
 ];
 yellow_11_2_main_stop_name = ["白河站","東山","西東原","南勢里<br />活動中心",'<span style="font-family:新細明體;">碧蓮寺</span>',"橫路<br />休息站"];
-yellow_11_2_main_stop_time_consume = [0, 5, 19, 25, 31, 35];
+yellow_11_2_main_stop_time_consume = [0, 5, 19, 26, 33, 37];
 yellow_11_2_important_stop = [0, 1, 5]; // 白河站河, 東山, 橫路休息站
 var Baihe = 0; // 白河站
 var Dongshan = 1; // 東山
 var West_Dongyuan = 2; // 西東原
 var Nanshih_Village = 3; // 南勢里活動中心
 var Bilian_Temple = 4; // 碧蓮寺
-yellow_11_2_time_go = [["06:20"],["09:00"],["14:40"],["16:45"]];
-yellow_11_2_time_return = [["07:00",[[Bilian_Temple,-1,Nanshih_Village,-1,Dongshan,1,Baihe,1]]],
-["09:40",[[Bilian_Temple,-1,Nanshih_Village,-1,Dongshan,1,Baihe,1]]],
-["15:20",[[Bilian_Temple,-1,Nanshih_Village,-1,Dongshan,1,Baihe,1]]],
-["17:25",[[Bilian_Temple,-1,Nanshih_Village,-1,Dongshan,1,Baihe,1]]]];
+yellow_11_2_time_go = [["06:20"],["08:40"],["14:40"],["16:45"]];
+yellow_11_2_time_return = [["07:05",[[Bilian_Temple,-1,Nanshih_Village,-1,Dongshan,1,Baihe,1]]],
+["09:20",[[Bilian_Temple,-1,Nanshih_Village,-1,Baihe,1]]],
+["15:20",[[Bilian_Temple,-1,Nanshih_Village,-1,Baihe,1]]],
+["17:30",[[Bilian_Temple,-1,Nanshih_Village,-1,Dongshan,1,Baihe,1]]]];
