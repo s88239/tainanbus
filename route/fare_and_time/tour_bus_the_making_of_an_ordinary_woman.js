b905fe0e1164a166828a9473425d9145002d77ab
Eric Kuo <s88239@hotmail.com>
2019-10-14 20:48:30 +0800

    更新時刻表：俗女養成記接駁車(改為每週五、六、日及國定假日行駛)

--- route/fare_and_time/tour_bus_the_making_of_an_ordinary_woman.js
+++ route/fare_and_time/tour_bus_the_making_of_an_ordinary_woman.js
@@ -10,8 +10,8 @@ tour_bus_the_making_of_an_ordinary_woman_fare = [
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-tour_bus_the_making_of_an_ordinary_woman_main_stop_name = ["高鐵嘉義站","菁寮"];
+tour_bus_the_making_of_an_ordinary_woman_main_stop_name = ["高鐵<br />嘉義站","菁寮"];
 tour_bus_the_making_of_an_ordinary_woman_main_stop_time_consume = [0, 25];
 tour_bus_the_making_of_an_ordinary_woman_important_stop = [0, 1]; // 高鐵嘉義站, 菁寮
-tour_bus_the_making_of_an_ordinary_woman_time_go = [["09:45"],["11:10"],["13:30",["H"]],["14:50"],["16:20",["H"]],["17:35"]];
-tour_bus_the_making_of_an_ordinary_woman_time_return = [["09:10"],["10:30"],["11:50",["H"]],["14:10"],["15:30",["H"]],["17:00"]];
+tour_bus_the_making_of_an_ordinary_woman_time_go = [["09:45"],["11:10"],["13:30"],["14:50"],["16:20"],["17:35"]];
+tour_bus_the_making_of_an_ordinary_woman_time_return = [["09:10"],["10:30"],["11:50"],["14:10"],["15:30"],["17:00"]];
