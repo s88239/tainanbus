83f89dae44f5a8ef0b089831d483024230b6f0c7
Eric Kuo <s88239@hotmail.com>
2019-10-19 14:40:31 +0800

    更新路線圖及時刻表：黃11-2

--- route/fare_and_time/yellow_11_2.js
+++ route/fare_and_time/yellow_11_2.js
@@ -6,7 +6,7 @@ yellow_11_2_interval_stop = [
 ["武嶺"], // 武嶺
 ["番子嶺","開門橋","萬應公廟","番子厝"], // 番子嶺
 ["斑芝花坑","斑花橋","柿子寮"], // 斑芝花坑
-["三塊厝","嶺南里路口","西東原","東原派出所","雙溪庄"], // 西東原
+["三塊厝","嶺南里路口","西東原","東原派出所","前雙溪","後雙溪","雙溪庄"], // 西東原
 ["北大洋","田寮子","大洋","後坑頭","南勢里活動中心"], // 北大洋
 ["南勢庄","岩坑"], // 南勢庄
 ["碧蓮寺","橫路"], // 碧蓮寺
@@ -25,16 +25,23 @@ yellow_11_2_fare = [
 [67,56,50,46,43,34,26,26,26,26],
 [74,63,56,53,50,40,31,26,26,26,26]
 ];
-yellow_11_2_main_stop_name = ["白河站","東山","西東原","南勢里<br />活動中心",'<span style="font-family:新細明體;">碧蓮寺</span>',"橫路<br />休息站"];
-yellow_11_2_main_stop_time_consume = [0, 5, 19, 26, 33, 37];
-yellow_11_2_important_stop = [0, 1, 5]; // 白河站河, 東山, 橫路休息站
+yellow_11_2_main_stop_name = ["白河站","東山","西東原","前雙溪","南勢里<br />活動中心",'<span style="font-family:新細明體;">碧蓮寺</span>',"橫路<br />休息站"];
+yellow_11_2_main_stop_time_consume = [0, 5, 19, 21, 26, 33, 37];
+yellow_11_2_important_stop = [0, 1, 6]; // 白河站, 東山, 橫路休息站
 var Baihe = 0; // 白河站
 var Dongshan = 1; // 東山
 var West_Dongyuan = 2; // 西東原
-var Nanshih_Village = 3; // 南勢里活動中心
-var Bilian_Temple = 4; // 碧蓮寺
-yellow_11_2_time_go = [["06:20"],["08:40"],["14:40"],["16:45"]];
-yellow_11_2_time_return = [["07:05",[[Bilian_Temple,-1,Nanshih_Village,-1,Dongshan,1,Baihe,1]]],
-["09:20",[[Bilian_Temple,-1,Nanshih_Village,-1,Baihe,1]]],
-["15:20",[[Bilian_Temple,-1,Nanshih_Village,-1,Baihe,1]]],
-["17:30",[[Bilian_Temple,-1,Nanshih_Village,-1,Dongshan,1,Baihe,1]]]];
+var Cian_Shuangsi = 3; // 前雙溪
+var Nanshih_Village = 4; // 南勢里活動中心
+var Bilian_Temple = 5; // 碧蓮寺
+var Henglu_Rest_Area = 6; // 橫路休息站
+yellow_11_2_time_go = [
+["06:20",[Nanshih_Village,[Bilian_Temple,-5,Henglu_Rest_Area,3]]],
+["08:40",[Cian_Shuangsi,[Henglu_Rest_Area,3]]],
+["14:40",[Nanshih_Village,[Bilian_Temple,-5,Henglu_Rest_Area,3]]],
+["16:45",[Cian_Shuangsi,[Henglu_Rest_Area,3]]]];
+yellow_11_2_time_return = [
+["06:55",[Cian_Shuangsi,[Bilian_Temple,-1,Nanshih_Village,-1,West_Dongyuan,1,Baihe,2]]],
+["09:20",[Nanshih_Village,[Bilian_Temple,-1,Cian_Shuangsi,-5,Baihe,2]]],
+["15:15",[Cian_Shuangsi,[Bilian_Temple,-1,Nanshih_Village,-1,West_Dongyuan,1,Baihe,2]]],
+["17:25",[Nanshih_Village,[Bilian_Temple,-1,Cian_Shuangsi,-5,Baihe,2]]]];
