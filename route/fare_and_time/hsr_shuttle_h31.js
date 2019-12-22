a9896a161b84ce5a0230a845a809f2d54bd24233
Eric Kuo <s88239@hotmail.com>
2019-12-22 23:36:06 +0800

    新增站位：黃幹線、橘10、橘10-1、棕10(增「南勢里」)；站位更名：H31(「永華站」更名為「小西門(大億麗緻)」)

--- route/fare_and_time/hsr_shuttle_h31.js
+++ route/fare_and_time/hsr_shuttle_h31.js
@@ -1,27 +1,20 @@
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-hsr_shuttle_h31_main_stop_name = ["原住民文化會館<br /><span style='color: red;'>(假日延駛)</span>","臺南市政府","永華站","延平郡王祠<br />(國立臺南大學)","大林新城","臺南航空站<br />(奇美幸福工廠)","奇美博物館","高鐵臺南站"];
+hsr_shuttle_h31_main_stop_name = ["原住民文化會館<br /><span style='color: red;'>(假日延駛)</span>","永華市政中<br />心(府前路)","小西門<br />(大億麗緻)","延平郡王祠<br />(國立臺南大學)","大林新城","臺南航空站<br />(奇美幸福工廠)","奇美博物館","高鐵臺南站"];
 hsr_shuttle_h31_main_stop_time_consume = [0, 10, 17, 25, 30, 40, 45, 60];
-hsr_shuttle_h31_important_stop = [0, 1, 5, 6, 7]; // 原住民文化會館, 臺南市政府, 臺南航空站(奇美幸福工廠), 奇美博物館, 高鐵臺南站
-var start_stop = 1; // 臺南市政府
-var end_stop = 7; // 高鐵臺南站
-hsr_shuttle_h31_time_go = [["05:10",start_stop,end_stop],["05:30",start_stop,end_stop],["06:00",start_stop,end_stop],["06:40",start_stop,end_stop],["07:00",start_stop,end_stop],["07:35",start_stop,end_stop],["07:50",start_stop,end_stop],["08:20",start_stop,end_stop],
-["08:35",start_stop,end_stop],["08:50",start_stop,end_stop],["09:20",start_stop,end_stop],["09:35",start_stop,end_stop],["09:40"],["10:20",start_stop,end_stop],["10:35",start_stop,end_stop],["10:40"],["11:20",start_stop,end_stop],
-["11:35",start_stop,end_stop],["11:40"],["12:20",start_stop,end_stop],["12:35",start_stop,end_stop],["12:40"],["13:20",start_stop,end_stop],["13:35",start_stop,end_stop],["13:40"],["14:20",start_stop,end_stop],["14:35",start_stop,end_stop],
-["14:40"],["15:20",start_stop,end_stop],["15:35",start_stop,end_stop],["15:40"],["16:20",start_stop,end_stop],["16:35",start_stop,end_stop],["16:40"],["17:20",start_stop,end_stop],["17:35",start_stop,end_stop],["17:40"],
-["18:20",start_stop,end_stop],["18:35",start_stop,end_stop],["18:40"],["19:20",start_stop,end_stop],["19:35",start_stop,end_stop],["19:40"],["20:20",start_stop,end_stop],["20:35",start_stop,end_stop],["20:40"],["21:20",start_stop,end_stop],
-["21:35",start_stop,end_stop],["21:50",start_stop,end_stop],["22:35",start_stop,end_stop],["22:55",start_stop,end_stop]];
-
-// The name of return stops are different
-hsr_shuttle_h31_main_stop_name_return = ["高鐵臺南站","奇美博物館","臺南航空站<br />(奇美幸福工廠)","大林新城","延平郡王祠<br />(國立臺南大學)","小西門<br />(大億麗緻)","臺南市政府","原住民文化會館<br /><span style='color: red;'>(假日延駛)</span>"];
-hsr_shuttle_h31_main_stop_time_consume_return = [0, 15, 20, 30, 35, 43, 50, 60];
-hsr_shuttle_h31_important_stop_return = [0, 1, 2, 6, 7]; // 高鐵臺南站, 奇美博物館, 臺南航空站(奇美幸福工廠), 臺南市政府, 原住民文化會館
-var start_stop_return = 0; // 高鐵臺南站
-var end_stop_return = 6; // 臺南市政府
-hsr_shuttle_h31_time_return = [["06:35",start_stop_return,end_stop_return],["07:00",start_stop_return,end_stop_return],["07:35",start_stop_return,end_stop_return],["07:50",start_stop_return,end_stop_return],["08:20",start_stop_return,end_stop_return],["08:35",start_stop_return,end_stop_return],["08:50"],["09:20",start_stop_return,end_stop_return],
-["09:35",start_stop_return,end_stop_return],["09:50"],["10:20",start_stop_return,end_stop_return],["10:35",start_stop_return,end_stop_return],["10:50"],["11:20",start_stop_return,end_stop_return],["11:35",start_stop_return,end_stop_return],["11:50"],["12:20",start_stop_return,end_stop_return],["12:35",start_stop_return,end_stop_return],
-["12:50"],["13:20",start_stop_return,end_stop_return],["13:35",start_stop_return,end_stop_return],["13:50"],["14:20",start_stop_return,end_stop_return],["14:35",start_stop_return,end_stop_return],["14:50"],["15:20",start_stop_return,end_stop_return],["15:35",start_stop_return,end_stop_return],
-["15:50"],["16:20",start_stop_return,end_stop_return],["16:35",start_stop_return,end_stop_return],["16:50"],["17:20",start_stop_return,end_stop_return],["17:35",start_stop_return,end_stop_return],["17:50"],["18:20",start_stop_return,end_stop_return],["18:35",start_stop_return,end_stop_return],["18:50"],
-["19:20",start_stop_return,end_stop_return],["19:35",start_stop_return,end_stop_return],["19:50"],["20:20",start_stop_return,end_stop_return],["20:35",start_stop_return,end_stop_return],["20:50",start_stop_return,end_stop_return],["21:20",start_stop_return,end_stop_return],["21:35",start_stop_return,end_stop_return],["21:50",start_stop_return,end_stop_return],
-["22:20",start_stop_return,end_stop_return],["22:40",start_stop_return,end_stop_return],["23:20",start_stop_return,end_stop_return],["23:35",start_stop_return,end_stop_return],["00:00",start_stop_return,end_stop_return]];
+hsr_shuttle_h31_important_stop = [0, 1, 5, 6, 7]; // 原住民文化會館, 永華市政中心(府前路), 臺南航空站(奇美幸福工廠), 奇美博物館, 高鐵臺南站
+var Tainan_City_Hall = 1; // 永華市政中心(府前路)
+var HSR_Tainan = 7; // 高鐵臺南站
+hsr_shuttle_h31_time_go = [["05:10",Tainan_City_Hall,HSR_Tainan],["05:30",Tainan_City_Hall,HSR_Tainan],["06:00",Tainan_City_Hall,HSR_Tainan],["06:40",Tainan_City_Hall,HSR_Tainan],["07:00",Tainan_City_Hall,HSR_Tainan],["07:35",Tainan_City_Hall,HSR_Tainan],["07:50",Tainan_City_Hall,HSR_Tainan],["08:20",Tainan_City_Hall,HSR_Tainan],
+["08:35",Tainan_City_Hall,HSR_Tainan],["08:50",Tainan_City_Hall,HSR_Tainan],["09:20",Tainan_City_Hall,HSR_Tainan],["09:35",Tainan_City_Hall,HSR_Tainan],["09:40"],["10:20",Tainan_City_Hall,HSR_Tainan],["10:35",Tainan_City_Hall,HSR_Tainan],["10:40"],["11:20",Tainan_City_Hall,HSR_Tainan],
+["11:35",Tainan_City_Hall,HSR_Tainan],["11:40"],["12:20",Tainan_City_Hall,HSR_Tainan],["12:35",Tainan_City_Hall,HSR_Tainan],["12:40"],["13:20",Tainan_City_Hall,HSR_Tainan],["13:35",Tainan_City_Hall,HSR_Tainan],["13:40"],["14:20",Tainan_City_Hall,HSR_Tainan],["14:35",Tainan_City_Hall,HSR_Tainan],
+["14:40"],["15:20",Tainan_City_Hall,HSR_Tainan],["15:35",Tainan_City_Hall,HSR_Tainan],["15:40"],["16:20",Tainan_City_Hall,HSR_Tainan],["16:35",Tainan_City_Hall,HSR_Tainan],["16:40"],["17:20",Tainan_City_Hall,HSR_Tainan],["17:35",Tainan_City_Hall,HSR_Tainan],["17:40"],
+["18:20",Tainan_City_Hall,HSR_Tainan],["18:35",Tainan_City_Hall,HSR_Tainan],["18:40"],["19:20",Tainan_City_Hall,HSR_Tainan],["19:35",Tainan_City_Hall,HSR_Tainan],["19:40"],["20:20",Tainan_City_Hall,HSR_Tainan],["20:35",Tainan_City_Hall,HSR_Tainan],["20:40"],["21:20",Tainan_City_Hall,HSR_Tainan],
+["21:35",Tainan_City_Hall,HSR_Tainan],["21:50",Tainan_City_Hall,HSR_Tainan],["22:35",Tainan_City_Hall,HSR_Tainan],["22:55",Tainan_City_Hall,HSR_Tainan]];
+hsr_shuttle_h31_time_return = [["06:35",HSR_Tainan,Tainan_City_Hall],["07:00",HSR_Tainan,Tainan_City_Hall],["07:35",HSR_Tainan,Tainan_City_Hall],["07:50",HSR_Tainan,Tainan_City_Hall],["08:20",HSR_Tainan,Tainan_City_Hall],["08:35",HSR_Tainan,Tainan_City_Hall],["08:50"],["09:20",HSR_Tainan,Tainan_City_Hall],
+["09:35",HSR_Tainan,Tainan_City_Hall],["09:50"],["10:20",HSR_Tainan,Tainan_City_Hall],["10:35",HSR_Tainan,Tainan_City_Hall],["10:50"],["11:20",HSR_Tainan,Tainan_City_Hall],["11:35",HSR_Tainan,Tainan_City_Hall],["11:50"],["12:20",HSR_Tainan,Tainan_City_Hall],["12:35",HSR_Tainan,Tainan_City_Hall],
+["12:50"],["13:20",HSR_Tainan,Tainan_City_Hall],["13:35",HSR_Tainan,Tainan_City_Hall],["13:50"],["14:20",HSR_Tainan,Tainan_City_Hall],["14:35",HSR_Tainan,Tainan_City_Hall],["14:50"],["15:20",HSR_Tainan,Tainan_City_Hall],["15:35",HSR_Tainan,Tainan_City_Hall],
+["15:50"],["16:20",HSR_Tainan,Tainan_City_Hall],["16:35",HSR_Tainan,Tainan_City_Hall],["16:50"],["17:20",HSR_Tainan,Tainan_City_Hall],["17:35",HSR_Tainan,Tainan_City_Hall],["17:50"],["18:20",HSR_Tainan,Tainan_City_Hall],["18:35",HSR_Tainan,Tainan_City_Hall],["18:50"],
+["19:20",HSR_Tainan,Tainan_City_Hall],["19:35",HSR_Tainan,Tainan_City_Hall],["19:50"],["20:20",HSR_Tainan,Tainan_City_Hall],["20:35",HSR_Tainan,Tainan_City_Hall],["20:50",HSR_Tainan,Tainan_City_Hall],["21:20",HSR_Tainan,Tainan_City_Hall],["21:35",HSR_Tainan,Tainan_City_Hall],["21:50",HSR_Tainan,Tainan_City_Hall],
+["22:20",HSR_Tainan,Tainan_City_Hall],["22:40",HSR_Tainan,Tainan_City_Hall],["23:20",HSR_Tainan,Tainan_City_Hall],["23:35",HSR_Tainan,Tainan_City_Hall],["00:00",HSR_Tainan,Tainan_City_Hall]];
