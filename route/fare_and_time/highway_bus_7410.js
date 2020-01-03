5ee49b2f906e40a4bd458e2bd948096172e04d6a
Eric Kuo <s88239@hotmail.com>
2020-01-04 01:09:42 +0800

    更新時刻表：黃9、棕10、7407、7408、7410；更新票價表：黃16-1

--- route/fare_and_time/highway_bus_7410.js
+++ route/fare_and_time/highway_bus_7410.js
@@ -1,16 +1,16 @@
-highway_bus_7410_interval_name = ["新營","埤寮","溪州寮","鹽水<br />分駐所","牛稠仔","義竹<br />社區","義竹","安溪<br />寮仔","後鎮仔","前東港","布袋港","遊客<br />中心","布袋<br />商港"];
+highway_bus_7410_interval_name = ["新營","埤寮","溪州寮","鹽水<br />分駐所","牛稠仔","厚生橋","義竹","安溪<br />寮仔","後鎮仔","前東港","布袋港","遊客<br />中心","布袋<br />商港"];
 highway_bus_7410_interval_stop = [
 ["新營站","圓環","三民路口","南新國中","新營高中","民治市政中心","新營高工","中正世家","電力公司","許丑"], // 新營
 ["埤寮","護鎮活動中心"], // 埤寮
-["溪州寮","南榮科技大學"], // 溪州寮
+["溪州寮","麗園社區","南榮科技大學"], // 溪州寮
 ["鹽水分駐所","鹽水國中"], // 鹽水分駐所
 ["岸內","義稠里","牛稠仔"], // 牛稠仔
-["義竹社區"], // 義竹社區
-["義竹國小","義竹","松仔腳","竹圍仔"], // 義竹
+["厚生橋"], // 厚生橋
+["義竹國小","義竹","松子腳","竹圍仔"], // 義竹
 ["四十九公廟","埤仔頭","安溪寮仔","磚廠"], // 安溪寮仔
 ["東後鎮仔","後鎮仔"], // 後鎮仔
 ["義布橋","平和社區","江山里","前東港","山寮仔"], // 前東港
-["布袋廟後","布袋鎮公所","布袋","布袋港"], // 布袋港
+["布袋廟後","布袋鎮公所","布袋鎮農會","布新橋"], // 布袋港
 ["布袋遊客中心"],  // 遊客中心
 ["布袋商港"] // 布袋商港
 ];
@@ -32,16 +32,16 @@ highway_bus_7410_fare = [
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-highway_bus_7410_main_stop_name = ["新營","鹽水<br />分駐所","義竹","前東港","布袋<br />遊客中心","布袋商港<br /><font color=\"red\">(4-9月延駛)</font>"];
-highway_bus_7410_main_stop_time_consume = [0,21,27,39,45,50];
-highway_bus_7410_important_stop = [0, 1, 4, 5]; // 新營, 鹽水分駐所, 布袋遊客中心, 布袋商港
-stop_0 = 0; // 新營
-stop_1 = 4; // 布袋遊客中心
+highway_bus_7410_main_stop_name = ["新營站","鹽水<br />分駐所","義竹","前東港","布袋<br />遊客中心","布袋商港<br /><font color=\"red\">(4-9月延駛)</font>"];
+highway_bus_7410_main_stop_time_consume = [0, 24, 30, 40, 48, 55];
+highway_bus_7410_important_stop = [0, 1, 4, 5]; // 新營站, 鹽水分駐所, 布袋遊客中心, 布袋商港
+var Sinying = 0; // 新營站
+var Budai = 4; // 布袋遊客中心
 highway_bus_7410_time_go = [
-["07:30",stop_0,stop_1],["08:20"],["08:50",['L']],["10:20",stop_0,stop_1],["12:10",stop_0,stop_1],["13:50",stop_0,stop_1],
-["15:00",stop_0,stop_1],["17:05",stop_0,stop_1],["18:00"],["18:50",stop_0,stop_1]
+["07:20"],["08:20"],["08:50",Sinying,Budai,['L',[Budai,2]]],["10:20",Sinying,Budai,[[Budai,2]]],["12:10",Sinying,Budai,[[Budai,2]]],["13:50",Sinying,Budai,[[Budai,2]]],
+["15:00",Sinying,Budai,[[Budai,2]]],["17:15"],["18:00",Sinying,Budai,[[Budai,2]]],["18:50",Sinying,Budai,[[Budai,2]]]
 ];
 highway_bus_7410_time_return = [
-["06:00",stop_1,stop_0],["08:20",stop_1,stop_0],["09:20"],["09:50",['L']],["11:10",stop_1,stop_0],["13:00",stop_1,stop_0],
-["14:40",stop_1,stop_0],["16:00",stop_1,stop_0],["18:05",stop_1,stop_0],["19:00"]
+["06:00",Budai,Sinying,[[Sinying,2]]],["08:25",[[Budai,-2,Sinying,2]]],["09:25",[[Budai,-2,Sinying,2]]],["09:50",Budai,Sinying,['L',[Sinying,2]]],["11:20",Budai,Sinying,[[Sinying,2]]],["13:10",Budai,Sinying,[[Sinying,2]]],
+["14:50",Budai,Sinying,[[Sinying,2]]],["16:00",Budai,Sinying,[[Sinying,2]]],["18:20",[[Budai,-2,Sinying,2]]],["19:00",Budai,Sinying,[[Sinying,2]]]
 ];
--- route/fare_and_time/yellow_9.js
+++ route/fare_and_time/yellow_9.js
@@ -23,15 +23,36 @@ yellow_9_fare = [
 // format = [time at the start stop] or
 // [time, other] or
 // [time, start_stop, end_stop, other]
-yellow_9_main_stop_name = ["新營","臺南市政府<br />民治中心","東興<br />國宅","南安<br />溪寮","後壁<br />高中","後壁<br />火車站","南靖<br />火車站","後寮村","高鐵<br />嘉義站","故宮南院<br /><span style='color:red;'>【二~日】</span>"];
-yellow_9_main_stop_time_consume = [0, 3, 7, 11, 16, 17, 24, 32, 40, 50];
-yellow_9_important_stop = [0, 5, 8, 9]; // 新營, 後壁火車站, 高鐵嘉義站, 故宮南院
-var start_stop = 0; // 新營
-var skip_stop = 1; // 臺南市政府民治中心
-var stop_1 = 2; // 東興國宅
-var stop_2 = 5; // 後壁火車站
+yellow_9_main_stop_name = ["新營站","臺南市政府<br />民治中心","臺南市<br />農會","東興<br />國宅","新營客運<br />管理中心","北安<br />溪寮","後壁<br />火車站","南靖<br />火車站","高鐵<br />嘉義站","故宮南院<br /><span style='color:red;'>【二~日】</span>"];
+yellow_9_main_stop_time_consume = [0, 4, 6, 9, 11, 16, 20, 27, 45, 55];
+yellow_9_important_stop = [0, 6, 8, 9]; // 新營站, 後壁火車站, 高鐵嘉義站, 故宮南院
+var Sinying = 0; // 新營站
+var Tainan_City_Hall_Minzhi_Center = 1; // 臺南市政府民治中心
+var Dongsing_Public_Housing = 3; // 東興國宅
+var North_Ansiliao = 5;
+var TRA_Houbi = 6; // 後壁火車站
+var TRA_Nanjing = 7;
 var hsr_chiayi = 8; // 高鐵嘉義站
-yellow_9_time_go = [["06:00",start_stop,hsr_chiayi,[skip_stop,'W']],["06:50",start_stop,hsr_chiayi],["07:00",start_stop,hsr_chiayi],["08:00"],["09:00"],["10:00"],["11:00"],["12:00"],["13:00"],["14:00"],["15:00"],
-["16:00"],["17:00",[[stop_1,5,stop_2,5]]],["17:40",start_stop,hsr_chiayi,['五']],["18:00",start_stop,hsr_chiayi],["18:40",start_stop,hsr_chiayi,[skip_stop,'五']],["19:00",start_stop,hsr_chiayi,[skip_stop]],["19:40",start_stop,hsr_chiayi,[skip_stop,'五']],["20:00",start_stop,hsr_chiayi,[skip_stop]],["20:40",start_stop,hsr_chiayi,[skip_stop,'五']]];
-yellow_9_time_return = [["07:15",hsr_chiayi,start_stop,['W']],["07:55",hsr_chiayi,start_stop],["08:25",hsr_chiayi,start_stop],["09:10",[[hsr_chiayi,5]]],["10:10",[[hsr_chiayi,5]]],["11:10",[[hsr_chiayi,5]]],["12:10",[[hsr_chiayi,5]]],["13:10",[[hsr_chiayi,5]]],["14:10",[[hsr_chiayi,5]]],["15:10",[[hsr_chiayi,5]]],["16:10",[[hsr_chiayi,5]]],
-["17:10",[[hsr_chiayi,5]]],["18:10",[skip_stop,[hsr_chiayi,5]]],["19:00",hsr_chiayi,start_stop,[skip_stop,'五']],["19:25",hsr_chiayi,start_stop,[skip_stop]],["20:00",hsr_chiayi,start_stop,[skip_stop,'五']],["20:25",hsr_chiayi,start_stop,[skip_stop]],["21:00",hsr_chiayi,start_stop,[skip_stop,'五']],["21:25",hsr_chiayi,start_stop,[skip_stop]],["22:00",hsr_chiayi,start_stop,[skip_stop,'五']]];
+yellow_9_time_go = [["06:00",Sinying,hsr_chiayi,[Tainan_City_Hall_Minzhi_Center]],["06:50",Sinying,hsr_chiayi,['W',[hsr_chiayi,10]]],["07:00",Sinying,hsr_chiayi],["08:00"],["09:00"],["10:00"],["11:00"],["12:00"],["13:00"],["14:00"],["15:00"],
+["16:00"],["17:00",[[Dongsing_Public_Housing,5,TRA_Houbi,5]]],["17:40",Sinying,hsr_chiayi,['五']],["18:00",Sinying,hsr_chiayi],["18:40",Sinying,hsr_chiayi,[Tainan_City_Hall_Minzhi_Center,'五']],["19:00",Sinying,hsr_chiayi,[Tainan_City_Hall_Minzhi_Center]],["19:40",Sinying,hsr_chiayi,[Tainan_City_Hall_Minzhi_Center,'五']],["20:00",Sinying,hsr_chiayi,[Tainan_City_Hall_Minzhi_Center]],["20:40",Sinying,hsr_chiayi,[Tainan_City_Hall_Minzhi_Center,'五']]];
+yellow_9_time_return = [
+["07:15",hsr_chiayi,Sinying,[[TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Tainan_City_Hall_Minzhi_Center,1,Sinying,3]]],
+["07:55",hsr_chiayi,Sinying,['W',[TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Tainan_City_Hall_Minzhi_Center,1,Sinying,3]]],
+["08:25",hsr_chiayi,Sinying,[[TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["09:05",[[hsr_chiayi,10,TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["10:05",[[hsr_chiayi,10,TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["11:05",[[hsr_chiayi,10,TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["12:05",[[hsr_chiayi,10,TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["13:05",[[hsr_chiayi,10,TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["14:05",[[hsr_chiayi,10,TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["15:05",[[hsr_chiayi,10,TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Tainan_City_Hall_Minzhi_Center,1,Sinying,3]]],
+["16:05",[[hsr_chiayi,10,TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Tainan_City_Hall_Minzhi_Center,1,Sinying,3]]],
+["17:05",[[hsr_chiayi,10,TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Tainan_City_Hall_Minzhi_Center,1,Sinying,3]]],
+["18:05",[Tainan_City_Hall_Minzhi_Center,[hsr_chiayi,10,TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["19:00",hsr_chiayi,Sinying,[Tainan_City_Hall_Minzhi_Center,'五',[TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["19:25",hsr_chiayi,Sinying,[Tainan_City_Hall_Minzhi_Center,[TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["20:00",hsr_chiayi,Sinying,[Tainan_City_Hall_Minzhi_Center,'五',[TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["20:25",hsr_chiayi,Sinying,[Tainan_City_Hall_Minzhi_Center,[TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["21:00",hsr_chiayi,Sinying,[Tainan_City_Hall_Minzhi_Center,'五',[TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["21:25",hsr_chiayi,Sinying,[Tainan_City_Hall_Minzhi_Center,[TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]],
+["22:00",hsr_chiayi,Sinying,[Tainan_City_Hall_Minzhi_Center,'五',[TRA_Nanjing,-5,TRA_Houbi,1,North_Ansiliao,-1,Dongsing_Public_Housing,1,Sinying,-1]]]];
